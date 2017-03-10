1ce0f1d9b919d496ea9e8a36a809da345f154f51
Igor Suvorov <hi@isuvorov.ru>
2017-03-10 16:53:37 +0400

    sourcemap production fix

--- tools/config.js
+++ tools/config.js
@@ -4,13 +4,12 @@ const ctx = {
   env: process.env.NODE_ENV,
   debug: !process.argv.includes('--release'),
   verbose: process.argv.includes('--verbose'),
-  sourcemap: 'eval',
   // webpackConfigDist: `${dirname}/build/webpack.config.js`,
   // webpackStats: 'verbose',
   dirname,
   pkg: require('../package.json'),
   deps: [
-    {
+    {`
       name: 'lego-starter-kit',
       path: fs.realpathSync(`${dirname}/node_modules/lego-starter-kit/src`),
       alias: 'lego-starter-kit',
