cc3cb8d9bfe6c3b5df836d7377246a7dcd2a0a24
Yukioru <obt195@gmail.com>
2017-03-10 14:46:42 +0400

    lsk

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,6 @@ NODE_ENV=production yarn run build &&
 cd ./build &&
 NODE_ENV=production yarn &&
 cd .. &&
-rsync -avz ./build/* s3:/projects/momentum/app &&
-ssh s3 'cd /projects/momentum && docker-compose stop && docker-compose up' &&
+rsync -avz ./build/* s3:/projects/lsk/app &&
+ssh s3 'cd /projects/lsk && docker-compose stop && docker-compose up' &&
 echo 'ok'
