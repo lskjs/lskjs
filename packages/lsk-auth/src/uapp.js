76104e216b4392733273c10553d2baf1f859a7a8
Igor Suvorov <hi@isuvorov.ru>
2017-05-10 07:52:35 +0400

    fix

--- packages/lsk-auth/src/uapp.js
+++ packages/lsk-auth/src/uapp.js
@@ -2,6 +2,7 @@ export default (ctx) => {
   return {
     init() {
       this.components = require('./components').default(ctx);
+      this.socials = require('./socials').default;// (ctx);
       // this.models = require('./models').default(ctx);
       this.stores = require('./mobx').default(ctx);
     },
