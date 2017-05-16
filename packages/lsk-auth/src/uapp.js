1ec418fd1573e610616f700a49bee5938cf30004
Igor Suvorov <hi@isuvorov.ru>
2017-05-16 15:13:32 +0400

    fix router

--- packages/lsk-auth/src/uapp.js
+++ packages/lsk-auth/src/uapp.js
@@ -5,6 +5,7 @@ export default (ctx) => {
       this.socials = require('./socials').default;// (ctx);
       // this.models = require('./models').default(ctx);
       this.stores = require('./mobx').default(ctx);
+      this.router = require('./router').default;
     },
   };
 };
