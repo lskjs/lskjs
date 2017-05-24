cce80df58e582759610b8ddd2426c94d6e345823
Igor Suvorov <hi@isuvorov.ru>
2017-05-24 07:44:52 +0400

    fix

--- deploy.sh
+++ deploy.sh
@@ -6,5 +6,6 @@ cd ./build &&
 NODE_ENV=production yarn &&
 cd .. &&
 rsync -avz ./build/* s3:/projects/lsk/app &&
+rsync -avz ./src/public/* s3:/projects/lsk/app/public &&
 ssh s3 'cd /projects/lsk && docker-compose stop && docker-compose up' &&
 echo 'ok'
