731261c46fbfc013f2387ac669bf220661b2ea19
Igor Suvorov <hi@isuvorov.ru>
2017-06-22 00:37:17 +0300

    fix resoursePoint bugs

--- src/CoreApp/helpers/wrapResoursePoint.js
+++ src/CoreApp/helpers/wrapResoursePoint.js
@@ -3,13 +3,22 @@ import asyncRouter from 'lego-starter-kit/utils/AsyncRouter';
 export default (ctx) => {
   return function wrapResoursePoint(resourse, params = {}) {
     const api = params.api || params.router || asyncRouter();
-    const prefix = params.prefix || '';
-    const middleware = params.middleware || function () {};
+    const prefix = params.prefix || null;
+    const middleware = params.middleware || null;
     const actions = params.actions || Object.keys(resourse); // ['list', 'get', 'create', 'update', 'remove'];
 
     actions.forEach((action) => {
       if (!resourse[action]) return;
-      api.all(`${prefix}/${action}`, middleware, resourse[action]);
+      const args = [
+        [prefix, action].filter(a => !!a).join('/'),
+        middleware,
+        resourse[action]
+      ].filter(a => !!a);
+
+      if (args[0] !== '/') {
+        args[0] = '/' + args[0];
+      }
+      api.all(...args);
     });
     return api;
   };
