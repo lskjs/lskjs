caf464970a2667b9868ec018ff0858b5950b40af
Nikita Savyolov <ga2mer@yandex.ru>
2017-12-02 19:48:48 +0300

    hmr

--- src/client.js
+++ src/client.js
@@ -3,6 +3,26 @@ import 'react-bootstrap-card/react-bootstrap-polyfill';
 import App from './App';
 import Uapp from './Uapp';
 import config from './config';
+
+// for react only update
+// if(module.hot) {
+//     module.hot.accept();
+// }
+//
+// if(!global.__APP) {
+//     ready();
+//     const app = new App({ config, Uapp });
+//     global.__APP = app;
+//     app.start();
+// } else {
+//     require('./Uapp/routes').default(global.__APP.uapp)
+//     global.__APP.hmrUpdate();
+// }
+
 ready();
 const app = new App({ config, Uapp });
 app.start();
+
+if (module.hot) {
+  module.hot.accept();
+}
