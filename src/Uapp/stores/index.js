13a9af634d1743d8c5e64e894af410b9d58e2022
Igor Suvorov <hi@isuvorov.ru>
2019-01-27 23:08:54 +0200

    quick fix

--- src/Uapp/stores/index.js
+++ src/Uapp/stores/index.js
@@ -1,17 +1,21 @@
 export default function () {
   return {
-    Store: require('./Store').default,
-    EntityStore: require('./EntityStore').default,
-    ListStore: require('./ListStore').default,
-    SelectStore: require('./SelectStore').default,
-    PageStore: require('./PageStore').default,
-    ListPageStore: require('./ListPageStore').default,
+    ...require('@lskjs/general/stores').default,
 
-    Entity: require('./EntityStore').default,
-    List: require('./ListStore').default,
-    Select: require('./SelectStore').default,
-    Page: require('./PageStore').default,
-    ListPage: require('./ListPageStore').default,
+
+    // Store: require('./Store').default,
+    // EntityStore: require('./EntityStore').default,
+    // ListStore: require('./ListStore').default,
+    // SelectStore: require('./SelectStore').default,
+    // PageStore: require('./PageStore').default,
+    // ListPageStore: require('./ListPageStore').default,
+
+    // Entity: require('./EntityStore').default,
+    // List: require('./ListStore').default,
+    // Select: require('./SelectStore').default,
+    // Page: require('./PageStore').default,
+    // ListPage: require('./ListPageStore').default,
+    // // ListPage: require('@lskjs/general/stores/ListPage').default,
 
   };
 }
