60760825c0d5461cb8d62ed24548ade5597d13d7
Igor Suvorov <hi@isuvorov.ru>
2019-02-01 00:02:10 +0200

    UserStore.update while setLocale

--- src/Uapp/helpers/setLocale.js
+++ src/Uapp/helpers/setLocale.js
@@ -3,25 +3,24 @@ import Cookies from 'js-cookie';
 export default async function (locale) {
   // TODO:
   // uapp.setState({ locale });
-
   if (locale && this.user && this.user?.locale !== locale) {
-    // console.log('user.editUser');
-    await this.user.editUser({
-      locale,
-    });
+    try {
+      const { User: UserStore } = this.stores;
+      await UserStore.update({
+        _id: this.user?._id,
+        locale,
+      });
+    } catch (err) {
+      console.error('uapp.setLocale', err); // eslint-disable-line no-console
+    }
   }
   if (Cookies && locale && Cookies.get('locale') !== locale) {
-    // console.log('Cookies');
     Cookies.set('locale', locale);
   }
   if (this.t('locale') !== locale) {
-    // console.log('t(locale');
     await this.initI18({
       lng: locale,
     });
-
-    // setTimeout(() => {
-    //   uapp.history.go(0);
-    // }, 500);
+    // setTimeout(() => {     //   uapp.history.go(0);    // }, 500);
   }
 }
