821e14b0fab1635f4aa782cedba9fee60545dac0
Igor Suvorov <hi@isuvorov.ru>
2019-03-06 22:06:28 +0200

    fix?

--- packages/lsk-mailer/src/server.js
+++ packages/lsk-mailer/src/server.js
@@ -1,26 +1,15 @@
 import get from 'lodash/get';
 import nodemailer from 'nodemailer';
 import inlineCss from 'nodemailer-juice';
-import Promise from 'bluebird';
-import getParser from './parser';
-
 export default (ctx) => {
-  const Parser = getParser(ctx);
   return class Mailer {
     getTemplates() {
       return require('./templates').default(ctx);
     }
     async init() {
-      // nodemailer
       this.config = get(ctx, 'config.mailer');
       this.templates = this.getTemplates();
       this.transporter = this.getTransporter();
-      this.parser = new Parser();
-      // parser
-      await this.parser.init();
-    }
-    async stop() {
-      await this.parser.stop();
     }
 
     getTransporter() {
@@ -33,25 +22,20 @@ export default (ctx) => {
         // нельзя прогонять через эту херню html который уже покрыт inline css
         this.transporter.use('compile', inlineCss());
       }
-      await this.parser.run();
     }
 
     // Отправить email
     async send(args) {
-      const {
-        to, template, params = {}, options = {}, t: tFunc, locale, ...otherProps
-      } = args;
-      const t = tFunc || ctx.i18 && ctx.i18.getFixedT && ctx.i18.getFixedT(locale || 'en') || (a => a);
+      const { to, template, params = {}, options = {}, t: tFunc, locale, ...otherProps } = args;
+      const t = tFunc ? tFunc : ctx.i18 && ctx.i18.getFixedT && ctx.i18.getFixedT(locale || 'en') || (a => a);
       try {
         if (!to) throw '!to email';
         if (!template) throw '!template';
         if (!this.transporter) throw '!transporter';
-        if (!this.templates[template]) throw `cant find email template ${template}`;
+        if (!this.templates[template]) throw 'cant find email template ' + template
         // Ищем шаблон
-        // Шаблон это класс, создаем экземпляр
-        const args2 = {
-          ctx, t, locale, params, ...otherProps,
-        };
+          // Шаблон это класс, создаем экземпляр
+        const args2 = { ctx, t, locale, params, ...otherProps };
         const emailTemplate = new this.templates[template](args2);
         // вызываем render
         const defaultOptions = emailTemplate.getOptions(args2);
@@ -71,9 +55,7 @@ export default (ctx) => {
     async sendText(to, subject, message, from) {
       if (!from) from = ctx.config.mailer.options.from;
       try {
-        return await this.transporter.sendMail({
-          to, subject, text: message, from,
-        });
+        return await this.transporter.sendMail({ to, subject, text: message, from });
       } catch (e) {
         throw e;
       }
