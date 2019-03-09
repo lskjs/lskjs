08f05337e475f4c8bd550571f7c051254ebdc8be
Igor Suvorov <hi@isuvorov.ru>
2019-03-09 13:23:23 +0200

    add lsk/upload

--- packages/lsk-modules/scripts/pack-bootstrap.sh
+++ packages/lsk-modules/scripts/pack-bootstrap.sh
@@ -1,8 +1,16 @@
 #!/bin/sh
+pwd && \
 rm -rf .babelrc && \
 cp ../../.babelrc . && \
-pwd && \
+rm -rf node_modules && \
 npm i && \
+rm -rf release && \
+rm -rf release && \
 rm -rf build && \
 mkdir -p build && \
-cp -R node_modules build
+cp -R package.json build && 
+cp -R package-lock.json build && 
+cp -R node_modules build && 
+cd build && \
+pwd && \
+npm link
