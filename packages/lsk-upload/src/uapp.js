8ae8aa64067fe1c297c220d85bb252adb265c284
Igor Suvorov <hi@isuvorov.ru>
2019-03-20 09:47:14 +0200

    lsk-upload done

--- packages/lsk-upload/src/uapp.js
+++ packages/lsk-upload/src/uapp.js
@@ -1,26 +1,28 @@
-export default (ctx) => {
-  return {
-    async uploadFile(file) {
-      const body = new FormData();  // eslint-disable-line
-      body.append('file', file);
-      const res = await ctx.api.fetch('/api/module/upload', {
-        method: 'POST',
-        headers: {
-          'Content-Type': '!',
-        },
-        body,
-      });
-      return res.data;
-    },
-    async uploadImage(body) {
-      const res = await ctx.api.fetch('/api/module/upload', {
-        method: 'POST',
-        headers: {
-          'Content-Type': '!',
-        },
-        body,
-      });
-      return res.data;
-    },
-  };
-};
+/* global window */
+
+export default uapp => ({
+  async uploadFile(file) {
+    if (typeof window === 'undefined') return false;
+    const { FormData } = window;
+    const body = new FormData();
+    body.append('file', file);
+    const res = await uapp.api.fetch('/api/module/upload', {
+      method: 'POST',
+      headers: {
+        'Content-Type': '!',
+      },
+      body,
+    });
+    return res.data;
+  },
+  async uploadImage(body) {
+    const res = await uapp.api.fetch('/api/module/upload', {
+      method: 'POST',
+      headers: {
+        'Content-Type': '!',
+      },
+      body,
+    });
+    return res.data;
+  },
+});
