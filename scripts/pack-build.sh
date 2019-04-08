e3135d9f77c420068a78d2e4f9e8947e14204a98
Igor Suvorov <hi@isuvorov.ru>
2019-04-08 18:05:50 +0300

    quick fix

--- scripts/pack-build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-mkdir -p ${DIST:-build} && \
-cp package.json ${DIST:-build}/ && \
-cp package-lock.json ${DIST:-build}/ && \
-cp README.md ${DIST:-build}/ && \
-# cp {package.json,package-lock.json,README.md} ${DIST:-build}/ && \
-npx babel src --out-dir ${DIST:-build} --source-maps inline --copy-files ${BUILD_PARAMS}
--- scripts/pack-release.sh
+++
@@ -1,4 +0,0 @@
-rm -rf release && \
-DEBUG=0 DIST=release `dirname "$0"`/pack-build.sh && \
-npm publish release
-# npm publish --access=public release
