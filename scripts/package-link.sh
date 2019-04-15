b8d53dec4aded9b652290b41b951dc3cbb516547
Igor Suvorov <hi@isuvorov.ru>
2019-04-15 20:36:05 +0300

    update link

--- scripts/package-link.sh
+++ scripts/package-link.sh
@@ -1,33 +1,31 @@
-cd build
-npm link
+cd build && npm link && cd ..
 npm link \
-  @lskjs/apiquery && \
-  @lskjs/auth && \
-  @lskjs/autobind && \
-  @lskjs/build && \
-  @lskjs/button && \
-  @lskjs/dashboard && \
-  @lskjs/db && \
-  @lskjs/dev && \
-  @lskjs/downloads && \
-  @lskjs/form && \
-  @lskjs/general && \
-  @lskjs/i18 && \
-  @lskjs/list && \
-  @lskjs/log && \
-  @lskjs/mailer && \
-  @lskjs/mobx && \
-  @lskjs/modal && \
-  @lskjs/module && \
-  @lskjs/permit && \
-  @lskjs/reactapp && \
-  @lskjs/scroll && \
-  @lskjs/server && \
-  @lskjs/tbot && \
-  @lskjs/theme && \
-  @lskjs/uapp && \
-  @lskjs/ui && \
-  @lskjs/upload && \
+  @lskjs/apiquery \
+  @lskjs/auth \
+  @lskjs/autobind \
+  @lskjs/build \
+  @lskjs/button \
+  @lskjs/dashboard \
+  @lskjs/db \
+  @lskjs/dev \
+  @lskjs/downloads \
+  @lskjs/form \
+  @lskjs/general \
+  @lskjs/i18 \
+  @lskjs/list \
+  @lskjs/log \
+  @lskjs/mailer \
+  @lskjs/mobx \
+  @lskjs/modal \
+  @lskjs/module \
+  @lskjs/permit \
+  @lskjs/reactapp \
+  @lskjs/scroll \
+  @lskjs/server \
+  @lskjs/tbot \
+  @lskjs/theme \
+  @lskjs/uapp \
+  @lskjs/ui \
+  @lskjs/upload \
   @lskjs/utils
-cd ..
-cp -R node_modules build && \
+rm -rf build/node_modules && cp -R node_modules build 
--- scripts/update-starter-kit.sh
+++ scripts/update-starter-kit.sh
@@ -1,4 +1,4 @@
 HOME='.'
-rsync -avE --progress  ../lib-starter-kit/ $HOME --exclude='*/'
+rsync -avE --progress  ../lib-starter-kit/ $HOME --exclude='*/' --exclude='lerna.json' --exclude='.huskyrc.json' --exclude='README.md'
 rsync -avE --progress  ../lib-starter-kit/.storybook $HOME
 rsync -avE --progress  ../lib-starter-kit/scripts/* $HOME/scripts
