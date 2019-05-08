16990770903cacda00f4791d7e86e739a8643049
Igor Suvorov <hi@isuvorov.ru>
2019-05-08 17:54:54 +0300

    fixes

--- scripts/package-bootstrap.sh
+++ scripts/package-bootstrap.sh
@@ -8,8 +8,8 @@ rm -rf build && mkdir -p build && \
 rm -rf node_modules && npm i && mkdir -p node_modules && \
 cp -R package.json build && \
 cp -R package-lock.json build && \
-npm run bootstrap:nodemodules
-npm run link:me
+npm run bootstrap:nodemodules && \
+npm run link:me && \
 
 echo "OK"
 
