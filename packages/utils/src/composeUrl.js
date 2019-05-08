e429202508ecd496d6ef9363b67c0f108d1acdd8
Igor Suvorov <hi@isuvorov.ru>
2019-05-08 10:11:05 +0300

    баг

--- packages/utils/src/composeUrl.js
+++ packages/utils/src/composeUrl.js
@@ -1,7 +1,7 @@
 import getQueryFromParams from './getQueryFromParams';
 
 export default function composeUrl({ qs = {}, url } = {}) {
-  let toOrHref = url;
+  let toOrHref = url || '';
   if (qs && Object.keys(qs).length) {
     if (toOrHref.indexOf('?') === -1) {
       toOrHref += '?';
