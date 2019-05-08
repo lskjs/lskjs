f3901519e726d6074eba0676decf337dcede3a37
Igor Suvorov <hi@isuvorov.ru>
2019-05-08 09:22:10 +0300

    quick fix

--- packages/utils/src/getQueryFromParams.js
+++ packages/utils/src/getQueryFromParams.js
@@ -8,7 +8,7 @@ export default function getQueryFromParams(params = {}, defaultParams) {
     .map((key) => {
       const val = params[key];
       if (isPlainObject(val) && isEmpty(val)) return null;
-      if (isEqual(val, defaultParams[key])) return null;
+      if (defaultParams && isEqual(val, defaultParams[key])) return null;
       if (val === null) return null;
       const val2 = typeof val === 'string' ? val : JSON.stringify(val);
       return [key, val2].join('=');
