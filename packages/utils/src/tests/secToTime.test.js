c2bf8de9a10355d5b6a4b8b29ce4d03751aafab9
Александр Можаев <nadymorigin@gmail.com>
2019-06-14 20:18:59 +0400

    secToTime tests

---
+++ packages/utils/src/tests/secToTime.test.js
@@ -0,0 +1,17 @@
+import { secToTime } from '../secToTime';
+
+test('test hours secToTime', () => {
+  expect(secToTime(3800)).toBe('01:03:20');
+});
+
+test('test minutes secToTime', () => {
+  expect(secToTime(380)).toBe('06:20');
+});
+
+test('test seconds secToTime', () => {
+  expect(secToTime(38)).toBe('00:38');
+});
+
+test('test 0 time secToTime', () => {
+  expect(secToTime(0)).toBe('00:00');
+});
