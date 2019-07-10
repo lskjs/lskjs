4a77973d0a09ce16c98af3baf45f5d241002df81
Igor Suvorov <hi@isuvorov.ru>
2019-07-10 12:34:55 +0300

    replace cdn to lskjs

--- packages/server/src/methods/getDocsTemplate.js
+++ packages/server/src/methods/getDocsTemplate.js
@@ -7,13 +7,13 @@ export default (ctx, params) => {
 ${JSON.stringify(params, null, 2)}
 -->
   <title>${params.name || ''} API docs</title>
-  <link rel="icon" type="image/png" href="//cdn.mgbeta.ru/swagger/images/favicon-32x32.png" sizes="32x32" />
-  <link rel="icon" type="image/png" href="//cdn.mgbeta.ru/swagger/images/favicon-16x16.png" sizes="16x16" />
-  <link href='//cdn.mgbeta.ru/swagger/css/typography.css' media='screen' rel='stylesheet' type='text/css'/>
-  <link href='//cdn.mgbeta.ru/swagger/css/reset.css' media='screen' rel='stylesheet' type='text/css'/>
-  <link href='//cdn.mgbeta.ru/swagger/css/screen.css' media='screen' rel='stylesheet' type='text/css'/>
-  <link href='//cdn.mgbeta.ru/swagger/css/reset.css' media='print' rel='stylesheet' type='text/css'/>
-  <link href='//cdn.mgbeta.ru/swagger/css/print.css' media='print' rel='stylesheet' type='text/css'/>
+  <link rel="icon" type="image/png" href="//lskjs.github.io/lskjs/swagger/images/favicon-32x32.png" sizes="32x32" />
+  <link rel="icon" type="image/png" href="//lskjs.github.io/lskjs/swagger/images/favicon-16x16.png" sizes="16x16" />
+  <link href='//lskjs.github.io/lskjs/swagger/css/typography.css' media='screen' rel='stylesheet' type='text/css'/>
+  <link href='//lskjs.github.io/lskjs/swagger/css/reset.css' media='screen' rel='stylesheet' type='text/css'/>
+  <link href='//lskjs.github.io/lskjs/swagger/css/screen.css' media='screen' rel='stylesheet' type='text/css'/>
+  <link href='//lskjs.github.io/lskjs/swagger/css/reset.css' media='print' rel='stylesheet' type='text/css'/>
+  <link href='//lskjs.github.io/lskjs/swagger/css/print.css' media='print' rel='stylesheet' type='text/css'/>
   <link href='https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css' media='print' rel='stylesheet' type='text/css'/>
   <style>
     .token{
@@ -30,25 +30,25 @@ ${JSON.stringify(params, null, 2)}
     }
   </style>
 
-  <script src='//cdn.mgbeta.ru/swagger/lib/object-assign-pollyfill.js' type='text/javascript'></script>
-  <script src='//cdn.mgbeta.ru/swagger/lib/jquery-1.8.0.min.js' type='text/javascript'></script>
-  <script src='//cdn.mgbeta.ru/swagger/lib/jquery.slideto.min.js' type='text/javascript'></script>
-  <script src='//cdn.mgbeta.ru/swagger/lib/jquery.wiggle.min.js' type='text/javascript'></script>
-  <script src='//cdn.mgbeta.ru/swagger/lib/jquery.ba-bbq.min.js' type='text/javascript'></script>
-  <script src='//cdn.mgbeta.ru/swagger/lib/handlebars-4.0.5.js' type='text/javascript'></script>
-  <script src='//cdn.mgbeta.ru/swagger/lib/lodash.min.js' type='text/javascript'></script>
-  <script src='//cdn.mgbeta.ru/swagger/lib/backbone-min.js' type='text/javascript'></script>
-  <script src='//cdn.mgbeta.ru/swagger/swagger-ui.js' type='text/javascript'></script>
-  <script src='//cdn.mgbeta.ru/swagger/lib/highlight.9.1.0.pack.js' type='text/javascript'></script>
-  <script src='//cdn.mgbeta.ru/swagger/lib/highlight.9.1.0.pack_extended.js' type='text/javascript'></script>
-  <script src='//cdn.mgbeta.ru/swagger/lib/jsoneditor.min.js' type='text/javascript'></script>
-  <script src='//cdn.mgbeta.ru/swagger/lib/marked.js' type='text/javascript'></script>
-  <script src='//cdn.mgbeta.ru/swagger/lib/swagger-oauth.js' type='text/javascript'></script>
+  <script src='//lskjs.github.io/lskjs/swagger/lib/object-assign-pollyfill.js' type='text/javascript'></script>
+  <script src='//lskjs.github.io/lskjs/swagger/lib/jquery-1.8.0.min.js' type='text/javascript'></script>
+  <script src='//lskjs.github.io/lskjs/swagger/lib/jquery.slideto.min.js' type='text/javascript'></script>
+  <script src='//lskjs.github.io/lskjs/swagger/lib/jquery.wiggle.min.js' type='text/javascript'></script>
+  <script src='//lskjs.github.io/lskjs/swagger/lib/jquery.ba-bbq.min.js' type='text/javascript'></script>
+  <script src='//lskjs.github.io/lskjs/swagger/lib/handlebars-4.0.5.js' type='text/javascript'></script>
+  <script src='//lskjs.github.io/lskjs/swagger/lib/lodash.min.js' type='text/javascript'></script>
+  <script src='//lskjs.github.io/lskjs/swagger/lib/backbone-min.js' type='text/javascript'></script>
+  <script src='//lskjs.github.io/lskjs/swagger/swagger-ui.js' type='text/javascript'></script>
+  <script src='//lskjs.github.io/lskjs/swagger/lib/highlight.9.1.0.pack.js' type='text/javascript'></script>
+  <script src='//lskjs.github.io/lskjs/swagger/lib/highlight.9.1.0.pack_extended.js' type='text/javascript'></script>
+  <script src='//lskjs.github.io/lskjs/swagger/lib/jsoneditor.min.js' type='text/javascript'></script>
+  <script src='//lskjs.github.io/lskjs/swagger/lib/marked.js' type='text/javascript'></script>
+  <script src='//lskjs.github.io/lskjs/swagger/lib/swagger-oauth.js' type='text/javascript'></script>
 
   <!-- Some basic translations -->
-  <!-- <script src='//cdn.mgbeta.ru/swagger/lang/translator.js' type='text/javascript'></script> -->
-  <!-- <script src='//cdn.mgbeta.ru/swagger/lang/ru.js' type='text/javascript'></script> -->
-  <!-- <script src='//cdn.mgbeta.ru/swagger/lang/en.js' type='text/javascript'></script> -->
+  <!-- <script src='//lskjs.github.io/lskjs/swagger/lang/translator.js' type='text/javascript'></script> -->
+  <!-- <script src='//lskjs.github.io/lskjs/swagger/lang/ru.js' type='text/javascript'></script> -->
+  <!-- <script src='//lskjs.github.io/lskjs/swagger/lang/en.js' type='text/javascript'></script> -->
 
   <script type="text/javascript">
     $(function () {
@@ -111,7 +111,7 @@ ${JSON.stringify(params, null, 2)}
 <body class="swagger-section">
 <div id='header'>
   <div class="swagger-ui-wrap">
-    <a id="logo" href="/"><img class="logo__img" alt="swagger" height="30" width="30" src="//cdn.mgbeta.ru/swagger/images/logo_small.png" /><span class="logo__title">${params.name || 'API Docs'}</span></a>
+    <a id="logo" href="/"><img class="logo__img" alt="swagger" height="30" width="30" src="//lskjs.github.io/lskjs/swagger/images/logo_small.png" /><span class="logo__title">${params.name || 'API Docs'}</span></a>
     <form id='api_selector'>
       <div class='input'><input placeholder="//example.com/api" id="input_baseUrl" name="baseUrl" type="text"/></div>
       <div id='auth_container'></div>
