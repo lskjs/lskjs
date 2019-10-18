43270ff9c309831c9a5e9c27f8405fb79445b68b
Igor Suvorov <hi@isuvorov.ru>
2019-10-18 19:51:48 +0300

    quick fix

--- packages/server-api/src/awaitHealthcheck.js
+++ packages/server-api/src/awaitHealthcheck.js
@@ -3,9 +3,9 @@ import isFunction from 'lodash/isFunction';
 import mapValues from 'lodash/mapValues';
 import some from 'lodash/some';
 
-export default async (incomeData) => {
+export default async (healthchecks) => {
   let data = {};
-  const promises = mapValues(incomeData, async (fn, key) => {
+  const promises = mapValues(healthchecks, async (fn, key) => {
     if (!isFunction(fn)) return fn;
     const start = Date.now();
     try {
@@ -13,7 +13,7 @@ export default async (incomeData) => {
       if (!result) return null;
       return Date.now() - start;
     } catch (err) {
-      console.error(`healthcheck(${key}) err`, err);  //eslint-disable-line
+      console.error(`healthchecks[${key}] err`, err);  //eslint-disable-line
       return null;
     }
   });
@@ -25,20 +25,24 @@ export default async (incomeData) => {
   const json = {
     CONTAINER_IMAGE: process.env.CONTAINER_IMAGE || global.CONTAINER_IMAGE,
     __VERSION: process.env.__VERSION || global.__VERSION,
-    __STAGE: process.env.__STAGE || global.__STAGE,
-    __INSTANCE: process.env.__INSTANCE || global.__INSTANCE,
-    __MASTER: process.env.__MASTER || global.__MASTER,
-    __SERVER__: process.env.__SERVER__ || global.__SERVER__,
-    __PROD__: process.env.__PROD__ || global.__PROD__,
+    ...mapValues({
+      __STAGE: process.env.__STAGE || global.__STAGE,
+      __INSTANCE: process.env.__INSTANCE || global.__INSTANCE,
+      __MASTER: process.env.__MASTER || global.__MASTER,
+      __SERVER__: process.env.__SERVER__ || global.__SERVER__,
+      __PROD__: process.env.__PROD__ || global.__PROD__,
+    }, Boolean),
     env: {
       __IMAGE__: global.__IMAGE__,
       __VERSION__: global.__VERSION__,
       __INSTANCE__: global.__INSTANCE__,
-      __STAGE__: global.__STAGE__,
-      __PROD__: global.__PROD__,
-      __DEV__: global.__DEV__,
-      __SERVER__: global.__SERVER__,
-      __CLIENT__: global.__CLIENT__,
+      ...mapValues({
+        __STAGE__: global.__STAGE__,
+        __PROD__: global.__PROD__,
+        __DEV__: global.__DEV__,
+        __SERVER__: global.__SERVER__,
+        __CLIENT__: global.__CLIENT__,
+      }, Boolean),
     },
   };
 
