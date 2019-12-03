90c372b2dcf5d37cf9f8f3e65c31e54bab6dae8d
Igor Suvorov <hi@isuvorov.ru>
2019-12-03 10:15:23 +0200

    refactor

--- packages/server/src/responses/pack.js
+++ packages/server/src/responses/pack.js
@@ -4,48 +4,53 @@ import get from 'lodash/get';
 export default ctx => function pack(raw = {}, info) {
   const res = this;
   const config = get(ctx, 'config.response', __DEV__ ? { log: false, debug: true } : {});
-  const isRaw = get(raw, '__raw', false) || get(raw, '__pack', false);
+  // const isRaw = get(raw, '__raw', false) || get(raw, '__pack', false);
   const isLog = Boolean(get(raw, '__log') === null ? config.log : get(raw, '__log'));
   const status = info.status || get(raw, '__status', null);
-  const data = raw ? omit(raw, ['__pack', '__raw', '__log', '__status']) : null;
-
-  const json = {
-    code: info.code,
-    message: info.message,
-    data,
-  };
+  let isJson;
+  let wrap;
+  let data;
+  if (typeof get(raw, '__raw') !== 'undefined') {
+    wrap = false;
+    isJson = false;
+    data = raw.__raw;
+  } else {
+    wrap = !get(raw, '__pack', false);
+    isJson = true;
+    if (raw === null) {
+      data = null;
+    } else {
+      data = omit(raw, ['__pack', '__raw', '__log', '__status']);
+    }
+  }
+  let result;
+  if (wrap) {
+    result = {
+      code: info.code,
+      message: info.message,
+      data,
+    };
+  } else {
+    result = data;
+  }
   if (status) {
     res.status(status);
   }
-  if (!data) {
-    if (isLog) {
-      ctx.log.trace('>>>>>>');
-      console.log(null); // eslint-disable-line no-console
-      ctx.log.trace('<<<<<<');
+  if (wrap && config.debug) {
+    if (info.err) {
+      result.err = info.err;
     }
-    return res.json(null);
-  }
-  if (isRaw) {
-    if (isLog) {
-      ctx.log.trace('>>>>>>');
-      console.log(data); // eslint-disable-line no-console
-      ctx.log.trace('<<<<<<');
+    if (info.debug) {
+      result.debug = info.debug;
     }
-    return res.send(data);
-  }
-  if (config.debug && info.err) {
-    json.err = info.err;
-  }
-  if (config.debug && info.debug) {
-    json.debug = info.debug;
-  }
-  if (config.debug && info.stack) {
-    json.stack = info.stack;
+    if (info.stack) {
+      result.stack = info.stack;
+    }
   }
   if (isLog) {
     ctx.log.trace('>>>>>>');
-    console.log(JSON.stringify(json, null, 2)); // eslint-disable-line no-console
+    console.log(isJson ? JSON.stringify(result, null, 2) : result); // eslint-disable-line no-console
     ctx.log.trace('<<<<<<');
   }
-  return res.json(json);
+  return isJson ? res.json(result) : res.send(result);
 };
