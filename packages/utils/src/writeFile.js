dc448d86cbbb63f22436b44f51dc3d8a1b23c118
Igor Suvorov <hi@isuvorov.ru>
2020-01-08 15:13:42 +0200

    bugfix

--- packages/utils/src/writeFile.js
+++ packages/utils/src/writeFile.js
@@ -1,12 +1,17 @@
 import fs from 'fs';
+import path from 'path';
 
-export default (file, data, { debug, ...options } = {}) => new Promise((resolve, reject) => {
-  fs.mkdir(file, { recursive: true }, (err) => {
+export default (filename, data, { debug, ...options } = {}) => new Promise((resolve, reject) => {
+
+  fs.mkdir(path.dirname(filename), { recursive: true }, (err) => {
     if (err) return reject(err);
-    return fs.writeFile(file, data, options, (err2) => {
+    return fs.writeFile(filename, data, options, (err2) => {
       if (err2) return reject(err2);
-      if (debug) console.log(` => ${file}`); // eslint-disable-line no-console
+      if (debug) console.log(` => ${filename}`); // eslint-disable-line no-console
       return resolve();
     });
   });
 });
+
+
+// node -e "require('./packages/utils/build/writeFile').default('/tmp/test2/test.txt', 'txt').catch(err => console.log(err))""
