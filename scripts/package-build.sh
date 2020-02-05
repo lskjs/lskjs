45f5ee8c2741b279756e852e57bf20489c6444cb
Igor Suvorov <hi@isuvorov.com>
2020-02-05 18:50:01 +0200

    feat: add ts deploy

--- scripts/package-build.sh
+++ scripts/package-build.sh
@@ -6,6 +6,7 @@ echo "BUILD_PARAMS=$BUILD_PARAMS"
 
 mkdir -p ${DIST} && \
 cp {package.json,package-lock.json,README.md} ${DIST}/ && \
+../../node_modules/typescript/bin/tsc --declaration --declarationMap --emitDeclarationOnly --outDir ${DIST} src/**.ts && \
 ../../node_modules/@babel/cli/bin/babel.js src --out-dir ${DIST} --source-maps both --extensions '.js,.jsx,.ts,.tsx' ${BUILD_PARAMS} && \
 echo "OK package-build"
 #  --minified
--- scripts/update-starter-kit.sh
+++ scripts/update-starter-kit.sh
@@ -1,8 +1,17 @@
 #!/usr/bin/env bash
 HOME='.'
-git pull ../lib-starter-kit
-rsync -avEp --progress  ../lib-starter-kit/ $HOME --exclude='*/' --exclude='lerna.json' --exclude='.huskyrc.json' --exclude='README.md' --exclude='CHANGELOG.md' --exclude='.env.js'
-rsync -avEp --progress  ../lib-starter-kit/.storybook $HOME
-rsync -avEp --progress  ../lib-starter-kit/scripts/* $HOME/scripts
+DIR=`pwd`
 
+# cd ../lib-starter-kit && git pull && cd $DIR && \
+rsync -avEp --progress  ../lib-starter-kit/ $HOME \
+  --exclude='*/' \
+  --exclude='.git' \
+  --exclude='coverage' \
+  --exclude='lerna.json' \
+  --exclude='.huskyrc.json' \
+  --exclude='README.md' \
+  --exclude='CHANGELOG.md'\
+  --exclude='.env.js' && \
+rsync -avEp --progress  ../lib-starter-kit/.storybook $HOME && \
+rsync -avEp --progress  ../lib-starter-kit/scripts/* $HOME/scripts && \
 echo "\n\n\nYou need:\nnpm install && npm run bootstrap && npm run update"
