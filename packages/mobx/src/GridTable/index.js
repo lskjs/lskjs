aa1fe27edf4dfcda5ed79c276ec5382ce471c318
Igor Suvorov <hi@isuvorov.com>
2020-02-10 12:43:53 +0200

    feat: move GridTable from lskjs to lskjs/ux

--- packages/mobx/src/GridTable/index.js
+++
@@ -1,4 +0,0 @@
-export { default as Table } from './GridTable';
-export { default as Row } from './GridTableRow';
-export { default as Col } from './GridTableCol';
-export const createIndex = () => { let index = 0; return () => index++ } //eslint-disable-line
