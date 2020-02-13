378e56456a066d77824b9e000b532fc77b37f997
Igor Suvorov <hi@isuvorov.com>
2020-02-13 10:10:45 +0200

    test(utils): add convertTimezone tests

--- packages/utils/tests/convertTimezone.test.js
+++ packages/utils/tests/convertTimezone.test.js
@@ -19,6 +19,14 @@ test('check set 18:00 spb', () => {
   expect(convertTimezone('2020-01-01T18:00:00+03:00', '+03:00', '+3:00')).toStrictEqual(new Date('2020-01-01T18:00:00+03:00'));
   expect(convertTimezone('2020-01-01T18:00:00+04:00', '+03:00', '+4:00')).toStrictEqual(new Date('2020-01-01T18:00:00+03:00'));
 });
+test('check set 15Z => 18 spb ', () => {
+  expect(convertTimezone('2020-01-01T15:00:00Z', '+02:00', '+3:00')).toStrictEqual(new Date('2020-01-01T15:00:00Z'));
+  // expect(convertTimezone('2020-01-01T18:00:00Z', '+03:00')).toStrictEqual(new Date('2020-01-01T18:00:00+03:00'));
+  // expect(convertTimezone('2020-01-01T18:00:00+01:00', '+03:00', '+1:00')).toStrictEqual(new Date('2020-01-01T18:00:00+03:00'));
+  // expect(convertTimezone('2020-01-01T18:00:00+02:00', '+03:00', '+2:00')).toStrictEqual(new Date('2020-01-01T18:00:00+03:00'));
+  // expect(convertTimezone('2020-01-01T18:00:00+03:00', '+03:00', '+3:00')).toStrictEqual(new Date('2020-01-01T18:00:00+03:00'));
+  // expect(convertTimezone('2020-01-01T18:00:00+04:00', '+03:00', '+4:00')).toStrictEqual(new Date('2020-01-01T18:00:00+03:00'));
+});
 // test('check date to localDate', () => {
 //   expect(convertTimezone('2020-01-01T18:00:00Z', '+03:00')).toStrictEqual(new Date('2020-01-01T15:00:00Z'));
 //   expect(convertTimezone('2020-01-01T18:00:00Z', '+03:00')).toStrictEqual(new Date('2020-01-01T18:00:00+03:00'));
