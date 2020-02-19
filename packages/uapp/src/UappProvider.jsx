55dfa1754613eaf405cbe629553c2da94f8a25b7
Igor Suvorov <hi@isuvorov.com>
2020-02-19 11:11:23 +0200

    fix: UappProvider this

--- packages/uapp/src/UappProvider.jsx
+++ packages/uapp/src/UappProvider.jsx
@@ -3,26 +3,27 @@ import PropTypes from 'prop-types';
 import { Provider as DefaultMobxProvider } from 'mobx-react';
 import { ThemeProvider as DefaultThemeProvider } from 'emotion-theming';
 
-function UappProvider({ uapp, children: rawChildren }) {
+const UappProvider = ({ uapp, children: rawChildren }) => {
   let children = <>{rawChildren}</>;
 
-  const { MobxProvider } = this;
-  const stores = (uapp && uapp.provide()) || {};
+  const { MobxProvider } = UappProvider;
+  const stores = (uapp && uapp.provide && uapp.provide()) || {};
   if (stores && Object.keys(stores).length && MobxProvider) {
     children = <MobxProvider {...stores}>{children}</MobxProvider>;
   }
 
-  const { ThemeProvider } = this;
+  const { ThemeProvider } = UappProvider;
   const { theme } = uapp;
   if (theme && ThemeProvider) {
     children = <ThemeProvider theme={theme}>{children}</ThemeProvider>;
   }
 
   return children;
-}
+};
 
 UappProvider.propTypes = {
-  uapp: PropTypes.instanceOf(PropTypes.object).isRequired,
+  // eslint-disable-next-line react/forbid-prop-types
+  uapp: PropTypes.object.isRequired,
   children: PropTypes.node.isRequired,
 };
 UappProvider.MobxProvider = DefaultMobxProvider;
