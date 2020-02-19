36580badb9005b154e84b633c0c4c41dc5359c2f
Igor Suvorov <hi@isuvorov.com>
2020-02-19 22:59:26 +0200

    fix(server): catchError middleware

--- packages/server/src/middlewares/catchError.js
+++ packages/server/src/middlewares/catchError.js
@@ -1,18 +1,21 @@
-export default () => (err, req, res) => {
-  if (req && req.log && req.log.error) {
-    req.log.error(
-      {
-        err,
-        query: req.query,
-        body: req.body,
-        headers: req.headers,
-      },
-      (err || {}).stack,
-    );
-  } else {
-    console.error(err); // eslint-disable-line no-console
-  }
-  res.status(err.status || 500);
-  if (res.err) return res.err(err);
-  return res.json(err);
-};
+/* eslint-disable no-unused-vars */
+export default () =>
+  function(err, req, res, next) {
+    if (req && req.log && req.log.error) {
+      req.log.error(
+        {
+          err,
+          query: req.query,
+          body: req.body,
+          headers: req.headers,
+        },
+        (err || {}).stack,
+      );
+    } else {
+      console.error(err); // eslint-disable-line no-console
+    }
+    res.status(err.status || 500);
+    if (res.err) return res.err(err);
+    // if (false) next()
+    return res.json(err);
+  };
