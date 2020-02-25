5479dc6d34b79c8c978c790874a9f4ef2b8cd590
Igor Suvorov <hi@isuvorov.com>
2020-02-25 10:05:15 +0200

    fix: path to toQs

--- packages/auth/src/utils/getAuthRedirectUrl.js
+++ packages/auth/src/utils/getAuthRedirectUrl.js
@@ -1,4 +1,4 @@
-import toQs from '../../../utils/src/toQs';
+import toQs from '@lskjs/utils/toQs';
 
 export default req =>
   `/auth/login?r=${encodeURIComponent(req.path + (req.query && Object.keys(req.query).length ? toQs(req.query) : ''))}`;
