2aa20d7530939816613501eccd2f33a113b6397c
Igor Suvorov <hi@isuvorov.com>
2020-03-05 17:29:34 +0200

    feat(server): add log-levels in errors

--- packages/server/src/middlewares/catchError.js
+++ packages/server/src/middlewares/catchError.js
@@ -1,18 +1,22 @@
 /* eslint-disable no-unused-vars */
+import get from 'lodash/get';
+
 export default () =>
   function(err, req, res, next) {
-    if (req && req.log && req.log.error) {
-      req.log.error(
-        {
-          err,
-          query: req.query,
-          body: req.body,
-          headers: req.headers,
-        },
-        (err || {}).stack,
-      );
-    } else {
-      console.error(err); // eslint-disable-line no-console
+    if (get(err, 'level', 'error') === 'error') {
+      if (req && req.log && req.log.error) {
+        req.log.error(
+          {
+            err,
+            query: req.query,
+            body: req.body,
+            headers: req.headers,
+          },
+          (err || {}).stack,
+        );
+      } else {
+        console.error(err); // eslint-disable-line no-console
+      }
     }
     res.status(err.status || 500);
     if (res.err) return res.err(err);
