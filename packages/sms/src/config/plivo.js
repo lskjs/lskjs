cf2c9f1efa66c9aba3dfa798946b2f279c2fd27e
Igor Suvorov <hi@isuvorov.com>
2020-03-13 10:28:10 +0200

    refactor(sms): plivo debug

---
+++ packages/sms/src/config/plivo.js
@@ -0,0 +1,6 @@
+export default {
+  provider: 'plivo',
+  phone: '447537157558',
+  authId: '###',
+  authToken: '###',
+};
--- packages/sms/src/transports/plivo.js
+++ packages/sms/src/transports/plivo.js
@@ -1,21 +1,25 @@
-// import axios from 'axios';
-// import plivo from 'plivo';
-const plivo = require('plivo');
+// const { Client }= require('plivo');
+import { Client } from 'plivo';
 
 export default class SmsPlivo {
   constructor(config) {
     this.config = config;
-    this.client = new plivo.Client(config.authId, config.authToken);
+    this.client = new Client(this.config.authId, this.config.authToken);
   }
   send({ phone, text, ...params } = {}) {
-    return this.client.messages.create(
-      this.config.phone, // src
-      phone, // dst
-      text, // text
-    ).then((response) => {
-      console.log(response);
-    }, (err) => {
-      console.error(err);
-    });
+    return this.client.messages
+      .create(
+        this.config.phone, // src
+        phone, // dst
+        text, // text
+      )
+      .then(
+        response => {
+          console.log(response);
+        },
+        err => {
+          console.error(err);
+        },
+      );
   }
 }
