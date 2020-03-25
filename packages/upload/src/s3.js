2f973c1189d9dfad47c729a416c9b4aafb3da621
Igor Suvorov <hi@isuvorov.com>
2020-03-25 14:40:51 +0200

    feat(upload): add s3 config in upload

--- packages/upload/src/s3.js
+++ packages/upload/src/s3.js
@@ -7,10 +7,12 @@ import UploadServerModule from './UploadServerModule';
 export default class S3UploadServerModule extends UploadServerModule {
   async init() {
     await super.init();
-    this.s3 = new aws.S3(this.config.s3);
+    const config = this.config.s3;
+    if (!config) throw '!config.upload.s3';
+    this.s3 = new aws.S3(config);
     this.storage = multerS3({
-      s3: this.s3,
-      bucket: 'buzzguru',
+      s3: config,
+      bucket: config.bucket,
       contentType: multerS3.AUTO_CONTENT_TYPE,
       acl: 'public-read',
       key: (req, file, cb) => {
