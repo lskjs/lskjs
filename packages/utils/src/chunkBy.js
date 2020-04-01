d632d5ef351d33794af0aa07602e7cb98d4ceea2
Igor Suvorov <hi@isuvorov.com>
2020-04-01 20:11:24 +0300

    feat(utils): chunkBy

---
+++ packages/utils/src/chunkBy.js
@@ -0,0 +1,26 @@
+import get from 'lodash/get';
+import set from 'lodash/set';
+import forEach from 'lodash/forEach';
+import groupBy from 'lodash/groupBy';
+
+export default (collection, predicate) => {
+  const chunks = [];
+  const counts = {};
+  const groups = groupBy(collection, predicate);
+
+  while (Object.keys(groups).length) {
+    forEach(groups, (group, key) => {
+      const count = get(counts, key, 0);
+      let chunk = chunks[count];
+      if (!chunk) {
+        chunk = [];
+        chunks[count] = chunk;
+      }
+      chunk.push(group.pop());
+      set(counts, key, count + 1);
+      if (group.length === 0) delete groups[key];
+    });
+  }
+
+  return chunks;
+};
---
+++ packages/utils/tests/chunkBy.test.js
@@ -0,0 +1,64 @@
+/* global test expect */
+import flatten from 'lodash/flatten';
+import chunkBy from '../src/chunkBy';
+
+test('check 0', () => {
+  const res = [
+    [
+      {
+        _id: 1,
+      },
+      {
+        _id: 2,
+      },
+      {
+        _id: 3,
+      },
+    ],
+    [
+      {
+        _id: 2,
+      },
+      {
+        _id: 3,
+      },
+    ],
+    [
+      {
+        _id: 3,
+      },
+    ],
+  ];
+  const array = flatten(res);
+  expect(chunkBy(array, a => a._id)).toStrictEqual(res);
+});
+test('check 1', () => {
+  const res = [
+    [
+      {
+        _id: 1,
+      },
+      {
+        _id: 2,
+      },
+      {
+        _id: 3,
+      },
+    ],
+    [
+      {
+        _id: 2,
+      },
+      {
+        _id: 3,
+      },
+    ],
+    [
+      {
+        _id: 3,
+      },
+    ],
+  ];
+  const array = flatten(res);
+  expect(chunkBy(array, '_id')).toStrictEqual(res);
+});
