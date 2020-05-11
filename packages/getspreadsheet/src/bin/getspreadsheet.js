6ccccc32f4a959d3238d57f7898674b7507da39d
Igor Suvorov <hi@isuvorov.com>
2020-05-11 17:03:24 +0300

    fix(getspreadsheet): add fake package json

--- packages/getspreadsheet/src/bin/getspreadsheet.js
+++ packages/getspreadsheet/src/bin/getspreadsheet.js
@@ -16,7 +16,7 @@ const parseAsync = promisify(parse);
 // ' https://docs.google.com/spreadsheets/d/1c9oYV4xIG32w7Y3Sh1V71zoQQBPhgxuXj2ea_tJU0RM',
 
 program
-  .version(require('../../package.json').version)
+  .version(require('../package.json').version)
   .command('getspreadsheet <url>')
   // .requiredOption('-u, --url <url>', 'url to the Google Spreadsheet document')
   .option('-o, --out <file>', 'path to json file with result')
