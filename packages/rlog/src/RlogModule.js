450021920bda96a36328e9285ff3b10df90b8528
Igor Suvorov <hi@isuvorov.com>
2020-05-12 09:40:57 +0300

    feat(rlog): add tests and prevent start()

--- packages/rlog/src/RlogModule.js
+++ packages/rlog/src/RlogModule.js
@@ -10,21 +10,27 @@ export default class RlogModule extends Module {
     this.logger = new Rlog(this.config);
   }
   send(...args) {
+    if (!this.logger) throw '!this.logger';
     return this.logger.send(...args);
   }
   trace(...args) {
+    if (!this.logger) throw '!this.logger';
     return this.logger.trace(...args);
   }
   success(...args) {
+    if (!this.logger) throw '!this.logger';
     return this.logger.success(...args);
   }
   warn(...args) {
+    if (!this.logger) throw '!this.logger';
     return this.logger.warn(...args);
   }
   error(...args) {
+    if (!this.logger) throw '!this.logger';
     return this.logger.error(...args);
   }
   fatal(...args) {
+    if (!this.logger) throw '!this.logger';
     return this.logger.fatal(...args);
   }
 }
---
+++ packages/rlog/test/rlog.js
@@ -0,0 +1,28 @@
+require('@lskjs/utils/polyfill');
+const Rlog = require('../build/Rlog').default;
+const RlogServerModule = require('../build/RlogServerModule').default;
+
+const main = async () => {
+  const rlog = new Rlog({
+    base: 'https://notify.isuvorov.com/notify',
+    project: `test`,
+  });
+  const res = await rlog.trace('Hello World – test');
+  console.log('rlog.trace result', res.data);
+
+  const app = {
+    config: {
+      rlog: {
+        base: 'https://notify.isuvorov.com/notify',
+        project: `test`,
+      },
+    },
+  };
+  const rlogModule = new RlogServerModule({ app });
+  await rlogModule.start();
+  const res2 = await rlogModule.trace('Hello World – test');
+  console.log('rlogModule.trace result', res2.data);
+};
+main().catch((err) => {
+  console.error('main error', err);
+});
