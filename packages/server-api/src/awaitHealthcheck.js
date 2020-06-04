d4cb7210215172309cfa0bc0ff29206c5f4544e5
Igor Suvorov <hi@isuvorov.com>
2020-06-04 13:34:35 +0300

    fix(server-api): healthchecks env

--- packages/server-api/src/awaitHealthcheck.js
+++ packages/server-api/src/awaitHealthcheck.js
@@ -20,38 +20,23 @@ export default async (healthchecks) => {
 
   data = await Promise.props(promises);
 
-  const status = some(data, a => a == null) ? 500 : 200;
-
-  const json = {
-    CONTAINER_IMAGE: process.env.CONTAINER_IMAGE || global.CONTAINER_IMAGE,
-    __VERSION: process.env.__VERSION || global.__VERSION,
-    ...mapValues({
-      __STAGE: process.env.__STAGE || global.__STAGE,
-      __INSTANCE: process.env.__INSTANCE || global.__INSTANCE,
-      __MASTER: process.env.__MASTER || global.__MASTER,
-      __SERVER__: process.env.__SERVER__ || global.__SERVER__,
-      __PROD__: process.env.__PROD__ || global.__PROD__,
-    }, Boolean),
-    env: {
-      __IMAGE__: global.__IMAGE__,
-      __VERSION__: global.__VERSION__,
-      __INSTANCE__: global.__INSTANCE__,
-      ...mapValues({
-        __STAGE__: global.__STAGE__,
-        __PROD__: global.__PROD__,
-        __DEV__: global.__DEV__,
-        __SERVER__: global.__SERVER__,
-        __CLIENT__: global.__CLIENT__,
-      }, Boolean),
-    },
-  };
+  const status = some(data, (a) => a == null) ? 500 : 200;
 
   return {
     __pack: true,
     __status: status,
     ok: true,
     date: new Date(),
-    ...mapValues(json, Boolean),
+    env: {
+      __IMAGE__: global.__IMAGE__,
+      __VERSION__: global.__VERSION__,
+      __INSTANCE__: global.__INSTANCE__,
+      __STAGE__: global.__STAGE__,
+      __PROD__: global.__PROD__,
+      __DEV__: global.__DEV__,
+      __SERVER__: global.__SERVER__,
+      __CLIENT__: global.__CLIENT__,
+    },
     data,
   };
 };
