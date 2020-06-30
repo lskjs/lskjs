4412c8c9e826ef4d88310acf045ed917e4c30704
Igor Suvorov <hi@isuvorov.com>
2020-06-30 13:48:09 +0300

    test(server): rewrite tests styles

--- packages/server/src/AsyncRouter/express-async-router.test.js
+++ packages/server/src/AsyncRouter/express-async-router.test.js
@@ -1,55 +1,48 @@
-/* global test expect */
-import AsyncRouter from '../AsyncRouter';
+/* global describe test expect */
 import express from 'express';
 import request from 'supertest';
+import AsyncRouter from '.';
 
 describe('Post Endpoints', () => {
-  it('test 1', async () => {
+  const middlewareUserId = (req, res, next) => {
+    req.userId = 123;
+    return next();
+  };
+  const middlewareReqId = (req, res, next) => {
+    req.reqId = 123;
+    return next();
+  };
+  test('test 1', async () => {
     const app = express();
     const router = AsyncRouter();
-    router.get('/test', async (req, res, next) => {
-      console.log('middleware');
-      next();
-    }, async (req, res) => {
+    router.get('/test', middlewareUserId, async (req, res) => {
       return res.send(200);
     });
     app.use('/', router);
-    const res = await request(app)
-      .get('/test');
-    expect(res.statusCode).toEqual(200)
+    const res = await request(app).get('/test');
+    expect(res.statusCode).toEqual(200);
     // expect(res.body).toHaveProperty('post')
-  })
-  it('test 2', async () => {
+  });
+  test('test 2', async () => {
     const app = express();
     const router = AsyncRouter();
-    router['all']('/test', (req, res, next) => {
-      console.log('middleware');
-      next();
-    }, async (req, res) => {
+    router.all('/test', middlewareUserId, async (req, res) => {
       return res.send(200);
     });
     app.use('/', router);
-    const res = await request(app)
-      .get('/test');
-    expect(res.statusCode).toEqual(200)
+    const res = await request(app).get('/test');
+    expect(res.statusCode).toEqual(200);
     // expect(res.body).toHaveProperty('post')
-  })
-  it('test 3', async () => {
+  });
+  test('test 3', async () => {
     const app = express();
     const router = AsyncRouter();
-    router['use']('/test', (req, res, next) => {
-      console.log('middleware');
-      next();
-    }, (req, res, next) => {
-      console.log('middleware2');
-      next();
-    }, async (req, res) => {
+    router.use('/test', middlewareUserId, middlewareReqId, async (req, res) => {
       return res.send(200);
     });
     app.use('/', router);
-    const res = await request(app)
-      .get('/test');
-    expect(res.statusCode).toEqual(200)
+    const res = await request(app).get('/test');
+    expect(res.statusCode).toEqual(200);
     // expect(res.body).toHaveProperty('post')
-  })
-})
+  });
+});
