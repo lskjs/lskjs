5cbfdf51096b73d00d24ddf66a1a226c22bc3162
Igor Suvorov <hi@isuvorov.com>
2020-08-06 00:28:11 +0300

    fix(utils): flattenKeys style

--- packages/utils/src/flattenKeys.js
+++ packages/utils/src/flattenKeys.js
@@ -2,7 +2,7 @@ import map from 'lodash/map';
 import flatten from 'lodash/flatten';
 import isCollection from './isCollection';
 
-export default function flattenKeys(object, keys = [], join = a => a.join('.')) {
+export default function flattenKeys(object, keys = [], join = (a) => a.join('.')) {
   if (isCollection(object)) {
     return flatten(
       map(object, (val, key) => {
