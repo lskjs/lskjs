73fc8ccdb0fcbeda6cbb1035e8348fac72fe5997
Александр Можаев <nadymorigin@gmail.com>
2020-09-03 14:19:45 +0400

    feat: add getPasswordEntropy

---
+++ packages/utils/src/getPasswordEntropy.js
@@ -0,0 +1,34 @@
+export default (password) => {
+  const upperCaseRegExp = new RegExp(/[A-Z]+/);
+  const digitRegExp = new RegExp(/[0-9]+/);
+  const lowerCaseRegExp = new RegExp(/[a-z]+/);
+  const specialCharRegExp = new RegExp(/[$&+,:;=?@#|'<>.^*()%!-]/);
+
+  let entropy = 10;
+  if (
+    password.length >= 8 &&
+    password.match(upperCaseRegExp) &&
+    password.match(lowerCaseRegExp) &&
+    password.match(digitRegExp) &&
+    password.match(specialCharRegExp)
+  ) {
+    entropy = 50;
+  } else if (
+    password.length >= 8 &&
+    ((password.match(upperCaseRegExp) && password.match(lowerCaseRegExp)) ||
+      (password.match(lowerCaseRegExp) && password.match(digitRegExp)) ||
+      (password.match(digitRegExp) && password.match(specialCharRegExp)) ||
+      (password.match(upperCaseRegExp) && password.match(digitRegExp)) ||
+      (password.match(upperCaseRegExp) && password.match(specialCharRegExp)) ||
+      (password.match(lowerCaseRegExp) && password.match(specialCharRegExp)))
+  ) {
+    entropy = 30;
+  } else if (
+    password.length < 8 &&
+    (password.match(upperCaseRegExp) || password.match(lowerCaseRegExp) || password.match(digitRegExp))
+  ) {
+    entropy = 10;
+  }
+
+  return entropy;
+};
