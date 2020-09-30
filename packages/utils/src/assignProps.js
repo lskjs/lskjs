31e7b56a5ad3ef35dfb41fcfcd4af97fec1cd7e5
Igor Suvorov <hi@isuvorov.com>
2020-09-30 09:33:26 +0300

    feat(utils): improve assignProps and importFn for array props

--- packages/utils/src/assignProps.js
+++ packages/utils/src/assignProps.js
@@ -1,16 +1,21 @@
-export default (target, appOrProps = {}) => {
+export default (target, ...arrayAppOrProps) => {
   let props = {};
-  if (appOrProps && appOrProps._module) {
-    if (appOrProps._module === 'app') {
-      props.app = appOrProps;
-    } else {
-      props.parent = appOrProps;
-      if (props.parent && props.parent.app && props.parent._module === 'app') {
-        props.app = props.parent.app;
+  arrayAppOrProps.forEach((appOrProps) => {
+    if (appOrProps && appOrProps._module) {
+      if (appOrProps._module === 'app') {
+        props.app = appOrProps;
+      } else {
+        props.parent = appOrProps;
+        if (props.parent && props.parent.app && props.parent._module === 'app') {
+          props.app = props.parent.app;
+        }
       }
+    } else {
+      props = {
+        ...props,
+        ...appOrProps,
+      };
     }
-  } else {
-    props = appOrProps;
-  }
+  });
   Object.assign(target, props);
 };
--- packages/utils/src/importFn.js
+++ packages/utils/src/importFn.js
@@ -1,12 +1,14 @@
 import undefault from './undefault';
 import isFunction from './isFunction';
+import isClass from './isClass';
+// import typeChecks from './typeChecks';
+// console.log('importFn2', fn, typeChecks(fn), Object.keys(fn), new String(fn), fn.name, fn.type, JSON.stringify(fn), fn.prototype, typeChecks(fn.prototype));
 
-export default async (fn) => {
-  let module;
-  if (isFunction(fn)) {
-    module = await fn();
-  } else {
-    module = await fn;
-  }
-  return undefault(module);
+const importFn = async (fn) => {
+  const pack = await (isFunction(fn) && !isClass(fn) ? fn() : fn);
+  if (!Array.isArray(pack)) return undefault(pack);
+  const [fn2, ...args] = pack;
+  return [await importFn(fn2), ...args];
 };
+
+export default importFn;
