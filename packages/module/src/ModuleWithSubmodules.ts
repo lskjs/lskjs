b639f0b1728f2cf6a556a58818baa007a2f559f7
Igor Suvorov <hi@isuvorov.com>
2021-01-18 16:12:11 +0200

    feat(module): add hadModule method

--- packages/module/src/ModuleWithSubmodules.ts
+++ packages/module/src/ModuleWithSubmodules.ts
@@ -2,6 +2,7 @@
 import arrayToObject from '@lskjs/utils/arrayToObject';
 import get from 'lodash/get';
 import omit from 'lodash/omit';
+import mapValues from 'lodash/mapValues';
 import asyncMapValues from '@lskjs/utils/asyncMapValues';
 import Err from '@lskjs/utils/Err';
 // import { setProps } from './utils/setProps';
@@ -9,9 +10,8 @@ import { ModuleWithEE } from './ModuleWithEE';
 import { createAsyncModule } from './utils/createAsyncModule';
 import { IModule, IModuleWithSubmodules, IModuleKeyValue, IAsyncModuleKeyValue } from './types';
 
-const filterWildcard = (array: string[], pattern: string): string[] => (
-  array.filter((name) => name.startsWith(pattern.substr(0, pattern.length - 1)))
-);
+const filterWildcard = (array: string[], pattern: string): string[] =>
+  array.filter((name) => name.startsWith(pattern.substr(0, pattern.length - 1)));
 
 export abstract class ModuleWithSubmodules extends ModuleWithEE implements IModuleWithSubmodules {
   __availableModules: IAsyncModuleKeyValue = {};
@@ -58,6 +58,22 @@ export abstract class ModuleWithSubmodules extends ModuleWithEE implements IModu
     };
   }
 
+  hasModule(nameOrNames: string | string[]): boolean | { [name: string]: boolean } {
+    console.log('@@@ hasModule', this.name, nameOrNames, JSON.stringify(this.__lifecycle))
+    if (typeof nameOrNames === 'string' && nameOrNames.endsWith('*')) {
+      // eslint-disable-next-line no-param-reassign
+      nameOrNames = filterWildcard(Object.keys(this.__availableModules), nameOrNames);
+    }
+    if (Array.isArray(nameOrNames)) {
+      return mapValues(arrayToObject(nameOrNames), (n: string) => this.hasModule(n) as boolean);
+    }
+    const name = nameOrNames;
+    console.log({ name }, this.__availableModules[name], Boolean(this.__availableModules[name]), Object.keys(this.__availableModules).includes(name));
+    return Boolean(this.__availableModules[name]);
+  }
+
+  __availableModules: IAsyncModuleKeyValue = {};
+
   async module(nameOrNames: string | string[], { run: isRun = true } = {}): Promise<IModule | IModuleKeyValue> {
     if (!this.__lifecycle.initStart)
       throw new Err('MODULE_INVALID_WORKFLOW_INIT', 'please init module first before .module()');
@@ -74,7 +90,10 @@ export abstract class ModuleWithSubmodules extends ModuleWithEE implements IModu
     if (this.debug) this.log.trace(`module(${name})`, isRun ? 'run' : undefined);
     if (this.__initedModules[name]) return this.__initedModules[name];
     const availableModule = this.__availableModules && this.__availableModules[name];
-    if (!availableModule) throw new Err('MODULE_INJECTING_NOT_FOUND', { data: { name } });
+    if (!availableModule)
+      throw new Err('MODULE_INJECTING_NOT_FOUND', `Module "${name}" not found in module ${this.name}`, {
+        data: { name },
+      });
     try {
       const moduleProps = await this.getModuleProps(name);
       const instance = await createAsyncModule(availableModule, moduleProps);
@@ -101,10 +120,9 @@ export abstract class ModuleWithSubmodules extends ModuleWithEE implements IModu
 
   private async __runModules(): Promise<void> {
     await asyncMapValues(this.__initedModules, (m: IModule) => {
-      const isNeedRun = (
+      const isNeedRun =
         this.__lifecycle.initFinish &&
-        (!this.__lifecycle.runStart || (this.__lifecycle.runStart && this.__lifecycle.stopFinish))
-      );
+        (!this.__lifecycle.runStart || (this.__lifecycle.runStart && this.__lifecycle.stopFinish));
       if (!isNeedRun) return;
       return m.__run();
     });
