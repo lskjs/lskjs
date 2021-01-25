99a6eeea19b27399b9c2a579e4b93c14c8696a86
Igor Suvorov <hi@isuvorov.com>
2021-01-25 02:06:07 +0200

    chore: update starter kit

--- scripts/update-starter-kit.sh
+++ scripts/update-starter-kit.sh
@@ -10,7 +10,7 @@ rsync -avEp --progress --ignore-existing ../lib-starter-kit/.lskjs.js $DIRTO && 
 rsync -avEp --progress --ignore-existing ../lib-starter-kit/README.md $DIRTO && \
 rsync -avEp --progress --ignore-existing ../lib-starter-kit/CHANGELOG.md $DIRTO && \
 rsync -avEp --progress --ignore-existing ../lib-starter-kit/.all-contributorsrc $DIRTO && \
-rsync -avEp --progress ../lib-starter-kit/scripts/assets $DIRTO/scripts/assets $D && \
+rsync -avEp --progress ../lib-starter-kit/scripts/assets/ $DIRTO/scripts/assets && \
 echo "===========================================" && \
 echo "        All OK, now you need todo:" && \
 echo "npm install && npm run bootstrap && npm run update" && \
