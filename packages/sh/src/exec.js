be51042cbb9c27a7b626fafcd9f70d44aca055a3
Igor Suvorov <hi@isuvorov.com>
2021-02-08 07:31:07 +0200

    feat(sh): improve exec and spawn

--- packages/sh/src/exec.js
+++ packages/sh/src/exec.js
@@ -1,29 +1,39 @@
 import trim from '@lskjs/utils/trimSpaces';
 import cp from 'child_process';
 
-function exec(command, options = {}) {
+// {
+// shell: true,
+// stdio: 'inherit',
+// }
+
+export function exec(command, options = {}) {
   const {
     trace = console.log, // eslint-disable-line no-console
     log = console.log, // eslint-disable-line no-console
     error = console.error, // eslint-disable-line no-console
+    ...otherOptions
     // cwd = process.cwd(),
   } = options;
 
   if (trace) trace('>>>', trim(command));
   return new Promise((resolve, reject) => {
-    const proc = cp.exec(command, { ...options }, (err, stdout, stderr) => {
+    const proc = cp.exec(command, { ...otherOptions }, (err, stdout, stderr) => {
       if (err) {
         reject({ err, stdout, stderr }); // eslint-disable-line prefer-promise-reject-errors
         return;
       }
       resolve({ stdout, stderr });
     });
-    proc.stdout.on('data', (data) => {
-      if (log) log(data.trim());
-    });
-    proc.stderr.on('data', (data) => {
-      if (error) error(data.trim());
-    });
+    if (proc.stdout) {
+      proc.stdout.on('data', (data) => {
+        if (log) log(data.trim());
+      });
+    }
+    if (proc.stderr) {
+      proc.stderr.on('data', (data) => {
+        if (error) error(data.trim());
+      });
+    }
   });
 }
 
--- packages/sh/src/spawn.js
+++ packages/sh/src/spawn.js
@@ -1,27 +1,41 @@
 import trim from '@lskjs/utils/trimSpaces';
-import { spawn } from 'child_process';
+import { spawn as nativeSpawn } from 'child_process';
 
-function exec(command, args = [], options = {}) {
+// {
+// shell: true,
+// stdio: 'inherit',
+// }
+
+export function spawn(command, args = [], options = {}) {
   const {
     trace = console.log, // eslint-disable-line no-console
     log = console.log, // eslint-disable-line no-console
     error = console.error, // eslint-disable-line no-console
     // cwd = process.cwd(),
+    ...otherOptions
   } = options;
 
   if (trace) trace('>>>', trim(command));
-  return new Promise((resolve) => {
-    const proc = spawn(command, args);
-    proc.stdout.on('data', (data) => {
-      const res = data.toString().trim();
-      if (log) log(res);
+  return new Promise((resolve, reject) => {
+    const proc = nativeSpawn(command, args, otherOptions);
+    if (proc.stdout) {
+      proc.stdout.on('data', (data) => {
+        const res = data.toString().trim();
+        if (log) log(res);
+      });
+    }
+    if (proc.stderr) {
+      proc.stderr.on('data', (data) => {
+        const res = data.toString().trim();
+        if (error) error(res);
+      });
+    }
+    proc.on('exit', (code) => {
+      if (!code) return resolve(proc);
+      // eslint-disable-next-line prefer-promise-reject-errors
+      reject({ proc, code });
     });
-    proc.stderr.on('data', (data) => {
-      const res = data.toString().trim();
-      if (error) error(res);
-    });
-    resolve(proc);
   });
 }
 
-export default exec;
+export default spawn;
