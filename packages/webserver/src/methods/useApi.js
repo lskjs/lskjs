e8d13095860ce91790a0ee95bf2f5657c487de20
Igor Suvorov <hi@isuvorov.com>
2021-04-09 16:04:13 +0300

    fix(webserver): fix useApi log

--- packages/webserver/src/methods/useApi.js
+++ packages/webserver/src/methods/useApi.js
@@ -4,19 +4,26 @@ import extractApi from './runRoutes/extractApi';
 import getRoutesTree from './runRoutes/getRoutesTree';
 import routesTreeToExpressRouter from './runRoutes/routesTreeToExpressRouter';
 
+const join = (items) => {
+  let finalMethod;
+  const finalPath = items
+    .map((item) => {
+      const [api, method] = item.split(' ').reverse();
+      finalMethod = method;
+      return api;
+    })
+    .join('');
+  return [finalMethod, finalPath].filter(Boolean).join('');
+};
+
 export default async function (Api) {
   if (!Api) throw '!Api';
   const path = '/';
-  // if (!this.getRoutesTree) this.getRoutesTree = getRoutesTree.bind(this);
-  // const log = (level, ...args) => (this.log ? this.log[level](...args) : console[level] && console[level](...args)); // eslint-disable-line no-console
   this.rootApi = new Api(this, { path, config: this.config.api });
   await this.rootApi.__run();
   this.routes = await getRoutesTree.bind(this.rootApi)(await extractApi(this.rootApi, {}), path);
   if (this.debug) {
-    this.log.trace(
-      'routes',
-      flattenKeys(this.routes, [], (a) => a.join('')),
-    );
+    this.log.trace('routes', flattenKeys(this.routes, [], join));
   }
   this.express.use(routesTreeToExpressRouter(this.routes));
   return this.express;
