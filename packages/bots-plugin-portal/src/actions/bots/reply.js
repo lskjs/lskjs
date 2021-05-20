763887080dda914f2efdd3863e982b2be6adb7df
Vlados <vlados.pochta@gmail.com>
2021-05-20 13:25:44 +0300

    fix(bots-plugin-portal): reply and sendMessage

--- packages/bots-plugin-portal/src/actions/bots/reply.js
+++ packages/bots-plugin-portal/src/actions/bots/reply.js
@@ -1,3 +1,8 @@
-export default async function reply({ text }) {
-  return this.bot.reply(this.ctx, text);
+export default async function reply(params) {
+  const { parent = {}, text, extra } = params;
+
+  const messageExtra = extra || parent.extra || {};
+  const messageText = text || parent.text || '';
+
+  return this.bot.reply(this.ctx, messageText, messageExtra);
 }
--- packages/bots-plugin-portal/src/actions/bots/sendMessage.js
+++ packages/bots-plugin-portal/src/actions/bots/sendMessage.js
@@ -2,6 +2,10 @@ import Bluebird from 'bluebird';
 
 export default async function sendMessage({ to, ...params }) {
   if (!to) throw '!to';
+  const { parent = {}, text, extra } = params;
+
+  const messageText = text || parent.text || '';
+  const messageExtra = extra || parent.extra || {};
   const chats = Array.isArray(to) ? to : [to];
-  return Bluebird.map(chats, async (chat) => this.bot.sendMessage(chat, params.text || params));
+  return Bluebird.map(chats, async (chat) => this.bot.sendMessage(chat, messageText, messageExtra));
 }
