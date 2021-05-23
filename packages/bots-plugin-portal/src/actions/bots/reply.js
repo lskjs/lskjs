3952b0eb48e7a80637642788a7c96195e547c830
Vlados <vlados.pochta@gmail.com>
2021-05-24 01:09:48 +0300

    fix(bots-plugin-portal): fix reply and sendMessage

--- packages/bots-plugin-portal/src/actions/bots/reply.js
+++ packages/bots-plugin-portal/src/actions/bots/reply.js
@@ -1,8 +1,7 @@
 export default async function reply(params) {
   const { parent = {}, text, extra } = params;
-
   const messageExtra = extra || parent.extra || {};
-  const messageText = text || parent.text || '';
+  const messageText = text || parent.text || 'undefined';
 
   return this.bot.reply(this.ctx, messageText, messageExtra);
 }
--- packages/bots-plugin-portal/src/actions/bots/sendMessage.js
+++ packages/bots-plugin-portal/src/actions/bots/sendMessage.js
@@ -4,7 +4,7 @@ export default async function sendMessage({ to, ...params }) {
   if (!to) throw '!to';
   const { parent = {}, text, extra } = params;
 
-  const messageText = text || parent.text || '';
+  const messageText = text || parent.text || 'undefined';
   const messageExtra = extra || parent.extra || {};
   const chats = Array.isArray(to) ? to : [to];
   return Bluebird.map(chats, async (chat) => this.bot.sendMessage(chat, messageText, messageExtra));
