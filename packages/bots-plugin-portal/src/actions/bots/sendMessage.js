7f61635797fa0f9eed1c997abf9c51f0e831bafe
Vlados <vlados.pochta@gmail.com>
2021-07-13 15:12:18 +0300

    fix(bots-plugin-portal): sendMessage parse_mode(markdown) param fixed

--- packages/bots-plugin-portal/src/actions/bots/sendMessage.js
+++ packages/bots-plugin-portal/src/actions/bots/sendMessage.js
@@ -1,14 +1,16 @@
 import Bluebird from 'bluebird';
 import isEmpty from 'lodash/isEmpty';
+import set from 'lodash/set';
 
 export default async function sendMessage({ to, ...params }) {
   if (!to && !this.ctx) throw '!to';
   if (!to) {
     to = this.bot.getMessageChatId(this.ctx);
   }
-  const { text, parent = {}, markdown = false } = params;
+  const { text, parent = {}, parseMode = '' } = params;
   if (isEmpty(parent)) parent.text = text;
+  if (parseMode) set(parent, 'extra.parse_mode', parseMode);
 
   const chats = Array.isArray(to) ? to : [to];
-  return Bluebird.map(chats, async (chat) => this.bot.sendMessage(chat, parent, parent.extra, markdown));
+  return Bluebird.map(chats, async (chat) => this.bot.sendMessage(chat, parent, parent.extra));
 }
