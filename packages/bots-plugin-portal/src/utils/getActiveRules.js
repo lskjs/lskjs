ac1a03a451628229e89c146c962dbcae1c646e0d
Vlados <vlados.pochta@gmail.com>
2021-07-22 14:55:39 +0300

    fix(bots-plugin-portal): add sticker criteria

--- packages/bots-plugin-portal/src/utils/getActiveRules.js
+++ packages/bots-plugin-portal/src/utils/getActiveRules.js
@@ -18,11 +18,14 @@ async function updateRules({ ctx, bot }) {
 
 export default async function getActiveRules({ ctx, bot } = {}) {
   const pack = {};
+  const message = bot.getMessage(ctx);
   pack.userId = bot.getUserId(ctx);
   pack.chatType = bot.getMessageChatType(ctx);
   pack.chatId = bot.getMessageChatId(ctx);
   pack.messageType = bot.getMessageType(ctx);
   pack.messageText = bot.getMessageText(ctx);
+  pack.stickerEmoji = message.sticker?.emoji;
+  pack.stickerSetName = message.sticker?.set_name;
   pack.nextRoute = bot.getNextRoute(ctx);
 
   const rules = await updateRules.call(this, { ctx, bot });
@@ -58,6 +61,18 @@ export default async function getActiveRules({ ctx, bot } = {}) {
     if (item instanceof RegExp) return item.test(pack.messageText);
     return item === pack.messageText;
   };
+  const checkStickerEmoji = (item) => {
+    if (Array.isArray(item)) return some(item, checkStickerEmoji);
+    if (isFunction(item) && !item(pack)) return false;
+    if (item instanceof RegExp) return item.test(pack.stickerEmoji);
+    return item === pack.stickerEmoji;
+  };
+  const checkStickerSetName = (item) => {
+    if (Array.isArray(item)) return some(item, checkStickerSetName);
+    if (isFunction(item) && !item(pack)) return false;
+    if (item instanceof RegExp) return item.test(pack.stickerSetName);
+    return item === pack.stickerSetName;
+  };
   const checkNextRoute = (item) => {
     if (Array.isArray(item)) return some(item, checkNextRoute);
     if (isFunction(item) && !item(pack)) return false;
@@ -68,7 +83,7 @@ export default async function getActiveRules({ ctx, bot } = {}) {
   const checkCriteria = (item) => {
     if (Array.isArray(item)) return some(item, checkCriteria);
     if (isFunction(item) && !item(pack)) return false;
-    const { userId, chatType, chatId, messageType, messageText, nextRoute } = item;
+    const { userId, chatType, chatId, messageType, messageText, stickerEmoji, stickerSetName, nextRoute } = item;
     if (userId && !checkUserId(userId)) {
       this.log.trace('!checkUserId');
       return false;
@@ -87,6 +102,14 @@ export default async function getActiveRules({ ctx, bot } = {}) {
     }
     if (messageText && !checkMessageText(messageText)) {
       this.log.trace('!checkMessageText');
+      return false;
+    }
+    if (stickerEmoji && !checkStickerEmoji(stickerEmoji)) {
+      this.log.trace('!checkStickerEmoji');
+      return false;
+    }
+    if (stickerSetName && !checkStickerSetName(stickerSetName)) {
+      this.log.trace('!checkStickerSetName');
       return false;
     }
     if (nextRoute && !checkNextRoute(nextRoute)) {
