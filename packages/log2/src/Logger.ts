c3b165613a1bc7fad1950f9f7448518190226ba5
Igor Suvorov <hi@isuvorov.com>
2021-07-30 10:36:25 +0300

    feat(log2): implement req loggger and prod serialization

--- packages/log2/src/Logger.ts
+++ packages/log2/src/Logger.ts
@@ -5,6 +5,7 @@ import { getCode, getMessage, isError } from '@lskjs/err/utils';
 import colors from 'colors/safe';
 
 import hashCode from './utils/hashCode';
+import leftPad from './utils/leftPad';
 import stringify from './utils/stringify';
 import tryParamParse from './utils/tryParamParse';
 
@@ -199,78 +200,27 @@ export class Logger implements ILogger {
     return theme.randoms[hashCode(key) % theme.randoms.length];
   }
   __logger(...args: any[]): void {
-    const stringedArgs = isDev ? args.map((a) => toString(a)) : args;
     // @ts-ignore
     // eslint-disable-next-line no-console
     if (console._log) {
       // @ts-ignore
       // eslint-disable-next-line no-console
-      console._log(...stringedArgs);
+      console._log(...args);
     } else {
       // eslint-disable-next-line no-console
-      console.log(...stringedArgs);
+      console.log(...args);
     }
   }
-  __log(level: LoggerLevelType, ...args: any[]): void {
-    const canPrint = this.getLevelPriority(level) >= this.getLevelPriority(env('LOG_LEVEL', ''));
-    if (!canPrint) return;
-
-    const time = new Date();
-    const logFormat = env('LOG_FORMAT', isDev ? 'debug' : 'bunyan');
-    if (['json', 'bunyan', 'logrus'].includes(logFormat)) {
-      const data: {
-        code?: any;
-        name?: string | null;
-        ns?: string | null;
-        level?: string | number;
-        time?: string | number;
-        msg?: string;
-        data?: any;
-      } = {
-        name: this.name,
-        ns: this.ns,
-      };
-
-      if (logFormat === 'bunyan') {
-        data.level = levelsPriority[level];
-      } else {
-        data.level = level;
-      }
-      if (logFormat === 'bunyan' || logFormat === 'logrus') {
-        data.time = time.toISOString();
-      } else {
-        data.time = +time;
-      }
-
-      // if (logFormat === 'bunyan' || logFormat === 'logrus') {
-      // data.msg = toString(args);
-      // }
-
-      if (env('LOG_DATA')) {
-        data.data = args;
-      }
-
-      if (!data.msg) data.msg = getMessage(args);
-
-      data.msg = args
-        .map((arg) => {
-          if (isError(arg)) {
-            if (!data.code) data.code = getCode(arg);
-            return getMessage(arg);
-          }
-          return toString(arg);
-        })
-        .join(' ');
-      // if (!data.msg) data.msg = toString(args);
-
-      this.__logger(omitNull(data));
-      return;
-    }
+  __getMarker(key: string): string {
+    const markers = ['•', '☼', '○', '♠', '♠', '♦', '♥'];
+    const marker = markers[hashCode(`2${key}`) % markers.length];
+    return this.color(this.getHashColor(key), marker);
+  }
+  __loggerPretty(level: LoggerLevelType, ...args: any[]): void {
     const res: string[] = [];
-    if (this.name === 'req') {
-      this.__logger(...res);
-      return;
-    }
+
+    let [mainArg] = args;
+    const [, ...otherArgs] = args;
 
     const envLogLevel = env('LOG_L', 'short');
     if (envLogLevel) {
@@ -283,8 +233,10 @@ export class Logger implements ILogger {
     const envLogNs = env('LOG_NS', 'true');
     const envLogName = env('LOG_NAME', 'true');
 
-    if (envLogNs || envLogName) {
-      let names: any[] = [];
+    let names: any[] = [];
+    if (this.name === 'req') {
+      names = [this.__getMarker(mainArg.reqId)];
+    } else if (envLogNs || envLogName) {
       if (envLogNs) {
         names = (this.ns || '').split('.');
       }
@@ -292,19 +244,38 @@ export class Logger implements ILogger {
         names.push(this.name);
       }
       names = names.filter(Boolean);
-      let str: string;
-      if (names.length) {
-        str = names.map((name: string) => this.color(this.getHashColor(name), name)).join(':');
-        if (str && envLogAlign) {
-          if (envLogAlign === 'left') str = str.padEnd(15);
-          if (envLogAlign === 'right') str = str.padStart(15);
-        }
-        res.push(str);
-      }
     }
 
-    let [mainArg] = args;
-    const [, ...otherArgs] = args;
+    if (names.length) {
+      let str: string;
+      str = names.map((name: string) => this.color(this.getHashColor(name), name)).join(':');
+      if (str && envLogAlign) {
+        if (envLogAlign === 'left') str = str.padEnd(15);
+        if (envLogAlign === 'right') str = str.padStart(15);
+      }
+      res.push(str);
+    }
+
+    if (this.name === 'req') {
+      const urlPad = -20;
+      let msg;
+      if (level === 'debug') {
+        msg = `${leftPad(mainArg.method, 4)} ${leftPad(mainArg.url, urlPad)} #${mainArg.reqId}`; // + '\x1b[33mYAUEBAN\x1b[0m AZAZA'
+      } else {
+        const time = (mainArg.duration || 0).toFixed(3);
+        const method = leftPad(mainArg.method, 4);
+        const length = mainArg.length || 0;
+        if (mainArg.method === 'WS') {
+          msg = `${method} ${leftPad(mainArg.url, urlPad)} #${mainArg.reqId} ${leftPad(time, 7)}ms `;
+        } else {
+          msg = `${method} ${leftPad(mainArg.url, urlPad)} #${mainArg.reqId} ${leftPad(mainArg.status, 3)} ${leftPad(
+            time,
+            7,
+          )}ms ${length}b `;
+        }
+      }
+      mainArg = msg;
+    }
 
     if (typeof mainArg === 'string') {
       if (['[', '<'].includes(mainArg[0]) && [']', '>'].includes(mainArg[mainArg.length - 1])) {
@@ -318,6 +289,88 @@ export class Logger implements ILogger {
     res.push(mainArg, ...otherArgs);
 
     this.__logger(...res);
+  }
+  __log(level: LoggerLevelType, ...args: any[]): void {
+    const canPrint = this.getLevelPriority(level) >= this.getLevelPriority(env('LOG_LEVEL', ''));
+    if (!canPrint) return;
+
+    const logFormat = env('LOG_FORMAT', isDev ? 'debug' : 'logrus');
+    if (!['json', 'bunyan', 'logrus'].includes(logFormat)) {
+      this.__loggerPretty(level, ...args);
+      return;
+    }
+
+    const time = new Date();
+    let data: {
+      code?: any;
+      name?: string | null;
+      ns?: string | null;
+      level?: string | number;
+      time?: string | number;
+      msg?: string;
+      data?: any;
+    } = {
+      name: this.name,
+      ns: this.ns,
+    };
+
+    if (logFormat === 'bunyan') {
+      data.level = levelsPriority[level];
+    } else {
+      data.level = level;
+    }
+    if (logFormat === 'bunyan' || logFormat === 'logrus') {
+      data.time = time.toISOString();
+    } else {
+      data.time = +time;
+    }
+
+    // if (logFormat === 'bunyan' || logFormat === 'logrus') {
+    // data.msg = toString(args);
+    // }
+
+    if (env('LOG_DATA')) {
+      data.data = args;
+    }
+
+    // if (!data.msg) data.msg = getMessage(args);
+    if (this.name === 'req') {
+      // console.log('REQQQQ', ...args);
+      data = {
+        ...data,
+        ...args[0],
+      };
+      // args
+      // .forEach((arg) => {
+
+      //   if (isError(arg)) {
+      //     if (!data.code) data.code = getCode(arg);
+      //     return getMessage(arg);
+      //   }
+      //   return toString(arg);
+      // })
+      // .join(' ');
+      // this.__logger(...res);
+      // return;
+    } else {
+      data.msg = args
+        .map((arg) => {
+          if (isError(arg)) {
+            if (!data.code) data.code = getCode(arg);
+            return getMessage(arg);
+          }
+          return toString(arg);
+        })
+        .join(' ');
+    }
+    data = omitNull(data);
+
+    // if (!data.msg) data.msg = toString(args)
+    if (isDev) {
+      this.__logger(data);
+      return;
+    }
+    this.__logger(toString(data));
   }
 }
 
