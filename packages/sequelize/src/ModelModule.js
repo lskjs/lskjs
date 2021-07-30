0917c04779832a239be3e16055e663a374c7acbf
Igor Suvorov <hi@isuvorov.com>
2021-07-30 20:22:23 +0300

    feat(sequelize): add models

---
+++ packages/sequelize/src/ModelModule.js
@@ -0,0 +1,32 @@
+import Module from '@lskjs/module';
+// @ts-ignore
+import importFn from '@lskjs/utils/importFn';
+
+export class ModelModule extends Module {
+  Model = null;
+  model = null;
+  dbName = 'db';
+  async createModel() {
+    // : Promise<any>
+    if (!this.dbName) throw '!this.dbName';
+    const db = await this.app.module(this.dbName, { run: true });
+    if (!db.client) {
+      this.log.error('!db.client');
+      throw '!db.client';
+    }
+    if (!this.Model) {
+      this.log.error('!Model');
+      throw '!Model';
+    }
+    const Model = await importFn(this.Model);
+
+    return Model(db);
+  }
+  async run() {
+    // : Promise<void>
+    await super.run();
+    this.model = await this.createModel();
+  }
+}
+
+export default ModelModule;
--- packages/sequelize/src/SequelizeServerModule.js
+++ packages/sequelize/src/SequelizeServerModule.js
@@ -1,54 +1,38 @@
 import Module from '@lskjs/module';
-import get from 'lodash/get';
-import merge from 'lodash/merge';
+import omit from 'lodash/omit';
 import Sequelize from 'sequelize';
 
 export default class SequelizeServerModule extends Module {
-  enabled = false;
-  Sequelize = Sequelize;
   config = {
-    options: {
-      dialect: 'postgres',
-      logging: false,
-      pool: {
-        max: 5,
-        min: 0,
-        idle: 100000,
-        acquire: 50000,
-        evict: 50000,
-        handleDisconnects: true,
-      },
+    dialect: 'postgres',
+    pool: {
+      max: 5,
+      min: 0,
+      idle: 100000,
+      acquire: 50000,
+      evict: 50000,
+      handleDisconnects: true,
     },
   };
 
-  async getConfig() {
-    return merge(
-      //
-      {},
-      await super.getConfig(),
-      get(this, 'config', {}),
-      get(this, '__config', {}),
-    );
-  }
+  Sequelize = Sequelize;
 
+  getOptions() {
+    const options = omit(this.config, ['uri']);
+    if (options.logging) options.logging = !!this.debug;
+    return options;
+  }
   async init() {
     await super.init();
     if (!this.config.uri) {
-      await this.log.warn('!uri ignore module');
+      await this.log.warn('!uri', 'ignore module');
       return;
     }
-    this.client = new Sequelize(this.config.uri, this.config.options);
-    this.models = this.getModels();
+    this.client = new this.Sequelize(this.config.uri, this.getOptions());
     await this.client.authenticate();
   }
-  getModels() {
-    return {};
-  }
   async run() {
-    if (!this.client) {
-      await this.log.warn('!client ignore module');
-      return;
-    }
+    if (!this.client) return;
     await this.client.sync();
   }
 }
