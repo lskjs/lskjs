d501c44ea3b8d7059d86fc5e3d3bf9d6d2772925
Igor Suvorov <hi@isuvorov.com>
2021-08-09 21:06:03 +0300

    refactor: some fixes

--- packages/i18/src/I18ClientModule.js
+++ packages/i18/src/I18ClientModule.js
@@ -1,5 +1,5 @@
 import I18MobxInstance from './I18MobxInstance';
-import I18Module from './I18Module';
+import { I18Module } from './I18Module';
 
 export class I18ClientModule extends I18Module {
   I18Instance = I18MobxInstance;
--- packages/i18/src/I18Module.js
+++ packages/i18/src/I18Module.js
@@ -18,7 +18,7 @@ export class I18Module extends Module {
     const { locales = [] } = this;
     if (!locales.includes(locale)) throw `!locales[${locale}]`;
     if (!this.instances[locale]) {
-      this.instances[locale] = await this.I18Instance.createAndRun({ config: { locale } });
+      this.instances[locale] = await this.I18Instance.start({ config: { locale } });
     }
     return this.instances[locale];
   }
