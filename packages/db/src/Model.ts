6c977746184ce124b901b73d864469f5d95540c5
Igor Suvorov <hi@isuvorov.com>
2021-08-10 10:30:23 +0300

    feat(db): add prepare and prepareOne

--- packages/db/src/Model.ts
+++ packages/db/src/Model.ts
@@ -1,3 +1,4 @@
+import Bluebird from 'bluebird';
 import forEach from 'lodash/forEach';
 import get from 'lodash/get';
 import set from 'lodash/set';
@@ -26,6 +27,32 @@ export class Model implements IModel {
   getState(key: string, def: any) {
     if (!key) return this;
     return get(this, key, def);
+  }
+
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  static async prepareOne(obj: any, params: any): Promise<any> {
+    return obj;
+  }
+  static async prepare(obj: any | any[], params: any = {}): Promise<any | any[]> {
+    // // console.log('PREPARE params.toObject', params.toObject);
+    // // console.log('params.toObject2@@@', params.toObject2);
+    const toObjectOne = (o) => {
+      if (!params.toObject2 || !Object.keys(params.toObject2).length) return o;
+      if (o && o.toObject) return o.toObject(params.toObject2);
+      return o;
+    };
+
+    let res;
+    if (Array.isArray(obj)) {
+      res = await Bluebird.map(obj, (o) => this.prepareOne(o, params));
+    } else {
+      res = await this.prepareOne(obj, params);
+    }
+
+    if (Array.isArray(res)) {
+      return res.map(toObjectOne);
+    }
+    return toObjectOne(res);
   }
 }
 
