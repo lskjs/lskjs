760a7a0b02b809d356c833a031dc98661b7e560a
Igor Suvorov <hi@isuvorov.com>
2021-08-10 16:37:28 +0300

    feat(fix):  transformResponse improvement

--- packages/mobx/src/stores2/Api.js
+++ packages/mobx/src/stores2/Api.js
@@ -1,15 +1,23 @@
+import Err from '@lskjs/err';
 import Module from '@lskjs/module';
 import axios from 'axios';
 
 export class Api extends Module {
-  transformResponse(res) {
-    return (res && res.data) || res;
+  transformResponse(raw) {
+    let res;
+    try {
+      res = JSON.parse(raw);
+    } catch (err) {
+      throw Err(`API_INVALID_JSON`, err);
+    }
+    if (res.code) throw Err(res);
+    return res.data;
   }
   async init() {
     await super.init();
     this.client = axios.create({
       baseURL: this.baseURL || this.base || '/api',
-      transformResponse: this.transformResponse.bind(this),
+      transformResponse: [this.transformResponse.bind(this)],
     });
   }
   fetch(...args) {
