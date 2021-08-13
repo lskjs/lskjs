00bcae7d9c29978f30e0bc126f24a6642b0c2293
Igor Suvorov <hi@isuvorov.com>
2021-08-13 14:30:48 +0300

    feat(mobx): improve ApiModule for create axios instances

--- packages/mobx/src/stores/Api.js
+++ packages/mobx/src/stores/Api.js
@@ -1,6 +1,5 @@
 import Err from '@lskjs/err';
 import Module from '@lskjs/module';
-import axios from 'axios';
 
 export class Api extends Module {
   transformResponse(raw) {
@@ -8,14 +7,15 @@ export class Api extends Module {
     try {
       res = JSON.parse(raw);
     } catch (err) {
-      throw Err(`API_INVALID_JSON`, err);
+      throw new Err(`API_INVALID_JSON`, err);
     }
     if (res.code) throw new Err(res);
     return res.data;
   }
   async init() {
     await super.init();
-    this.client = axios.create({
+    const api = await this.app.module('api');
+    this.client = await api.createClient({
       baseURL: this.baseURL || this.base || '/api',
       transformResponse: [this.transformResponse.bind(this)],
     });
