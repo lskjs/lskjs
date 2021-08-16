87142a958a48715a693d0baec2578aa1c77b8091
Igor Suvorov <hi@isuvorov.com>
2021-08-16 13:57:51 +0300

    feat(grant): add getGrantCache method

---
+++ packages/grant/src/Api/getGrantCache.js
@@ -0,0 +1,26 @@
+import Err from '@lskjs/err';
+
+export async function getGrantCache(req, rules) {
+  const grant = await this.app.module('grant');
+  const userId = req.user ? req.user._id : null;
+  const grantCache = grant.getCache(rules, {
+    user: req.user,
+    userId,
+    cache: grant.__cache.fork({ prefix: userId || 'guest' }),
+  });
+
+  grantCache.checkAccess = async function (action) {
+    const can = await this.can(action);
+    if (can) return;
+    throw new Err('grant.access', {
+      status: 403,
+      data: {
+        action,
+      },
+    });
+  };
+
+  return grantCache;
+}
+
+export default getGrantCache;
