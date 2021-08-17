3d63e613319a2dcc812ddff140781a63cd689ecf
Igor Suvorov <hi@isuvorov.com>
2021-08-17 13:24:22 +0300

    fix(mailer): mailer and i18 instance

--- packages/i18/src/I18Module.js
+++ packages/i18/src/I18Module.js
@@ -10,22 +10,32 @@ export class I18Module extends Module {
   instances = {};
 
   async getT(locale = this.locale) {
+    this.log.warn('getT deprecated');
     const instance = await this.instance(locale);
     return instance.t.bind(instance);
   }
-
-  async instance(locale = this.locale) {
+  hasInstance(locale) {
     if (!locale) throw new Err('!locale');
     const { locales = [] } = this;
-    if (!locales.includes(locale)) {
-      throw new Err('!locale', 'cant find current locale in locales', { data: { locale, locales } });
+    if (!locales.includes(locale)) return false;
+    return true;
+  }
+  async instance(locale = this.locale, throwError = true) {
+    if (!locale) throw new Err('!locale');
+    const { locales = [] } = this;
+    if (!this.hasInstance(locale)) {
+      if (throwError || !this.hasInstance(this.locale)) {
+        throw new Err('!locale', 'cant find current locale in locales', { data: { locale, locales } });
+      }
+      // eslint-disable-next-line no-param-reassign
+      locale = this.locale;
     }
-    if (!this.instances[locale]) {
-      this.instances[locale] = await this.I18Instance.start({
-        config: { ...(this.config || {}), locale },
-      });
-    }
-    return this.instances[locale];
+    if (this.instances[locale]) return this.instances[locale];
+    const instance = await this.I18Instance.start({
+      config: { ...(this.config || {}), locale },
+    });
+    this.instances[locale] = instance;
+    return instance;
   }
 
   async init() {
