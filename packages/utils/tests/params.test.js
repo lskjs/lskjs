bbbba4b025b7283f0278fe41ec8743c6f38b1e7f
Igor Suvorov <hi@isuvorov.com>
2021-08-20 16:57:05 +0300

    feat(utils): add toArray in params

--- packages/utils/tests/params.test.js
+++ packages/utils/tests/params.test.js
@@ -1,5 +1,5 @@
 /* global test expect */
-import { params, toBoolean, toInt, toNumber } from '../src/params';
+import { params, toArray, toBoolean, toInt, toNumber } from '../src/params';
 
 test('toBoolean(true) === true', () => {
   const obj = true;
@@ -84,6 +84,55 @@ test('toInt("123.34") === 123', () => {
   expect(toInt(obj)).toStrictEqual(res);
 });
 
+test('toArray("a") === ["a"]', () => {
+  const obj = 'a';
+  const res = ['a'];
+
+  expect(toArray(obj)).toStrictEqual(res);
+});
+test('toArray(["a"]) === ["a"]', () => {
+  const obj = ['a'];
+  const res = ['a'];
+
+  expect(toArray(obj)).toStrictEqual(res);
+});
+test('toArray("a,b") === ["a", "b"]', () => {
+  const obj = 'a,b';
+  const res = ['a', 'b'];
+
+  expect(toArray(obj)).toStrictEqual(res);
+});
+test('toArray(1) === [1]', () => {
+  const obj = '1';
+  const res = [1];
+
+  expect(toArray(obj)).toStrictEqual(res);
+});
+test('toArray(1,2) === [1,2]', () => {
+  const obj = '1,2';
+  const res = [1, 2];
+
+  expect(toArray(obj)).toStrictEqual(res);
+});
+test('toArray(null) === null', () => {
+  const obj = null;
+  const res = null;
+
+  expect(toArray(obj)).toStrictEqual(res);
+});
+test('toArray("") === []', () => {
+  const obj = '';
+  const res = [];
+
+  expect(toArray(obj)).toStrictEqual(res);
+});
+test('toArray([1]) === [1]', () => {
+  const obj = [1];
+  const res = [1];
+
+  expect(toArray(obj)).toStrictEqual(res);
+});
+
 test('params', () => {
   const data = {
     a: 'true',
@@ -92,7 +141,9 @@ test('params', () => {
     d: '2020-02-02',
     e: '',
     // f: 'true',
-    j: '{"a": true, "b": 123.45}',
+    g: '{"a": true, "b": 123.45}',
+    h: 'a,b,c',
+    i: '1,2,3',
   };
   const schema = {
     a: 'boolean',
@@ -107,19 +158,23 @@ test('params', () => {
       type: 'boolean',
       default: true,
     },
-    j: 'json',
+    g: 'json',
+    h: 'array',
+    i: 'array',
   };
   const res = {
     a: true,
     b: 123.45,
     c: 123,
     d: new Date('2020-02-02'),
-    e: false,
+    e: true,
     f: true,
-    j: {
+    g: {
       a: true,
       b: 123.45,
     },
+    h: ['a', 'b', 'c'],
+    i: [1, 2, 3],
   };
   expect(params(data, schema)).toStrictEqual(res);
 });
