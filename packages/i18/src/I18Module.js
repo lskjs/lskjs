876a6da67c1429e9b99180c8e6743f148eeeb653
Igor Suvorov <hi@isuvorov.com>
2021-09-01 13:54:33 +0300

    feat(i18): instance check locales array

--- packages/i18/src/I18Module.js
+++ packages/i18/src/I18Module.js
@@ -20,16 +20,21 @@ export class I18Module extends Module {
     if (!locales.includes(locale)) return false;
     return true;
   }
-  async instance(locale = this.locale, throwError = true) {
-    if (!locale) throw new Err('!locale');
+  async instance(localeOrLocales = this.locale, throwError = true) {
+    if (!localeOrLocales) throw new Err('!locale');
     const { locales = [] } = this;
-    if (!this.hasInstance(locale)) {
-      if (throwError || !this.hasInstance(this.locale)) {
-        throw new Err('!locale', 'cant find current locale in locales', { data: { locale, locales } });
-      }
-      // eslint-disable-next-line no-param-reassign
-      locale = this.locale;
+    // eslint-disable-next-line no-param-reassign
+    if (!Array.isArray(localeOrLocales)) localeOrLocales = [localeOrLocales];
+
+    // eslint-disable-next-line no-param-reassign
+    localeOrLocales = localeOrLocales.filter((locale) => this.hasInstance(locale));
+
+    if (!localeOrLocales.length) {
+      if (throwError)
+        throw new Err('!locale', 'cant find current locale in locales', { data: { locale: localeOrLocales, locales } });
+      return null;
     }
+    const locale = localeOrLocales[0];
     if (this.instances[locale]) return this.instances[locale];
     const instance = await this.I18Instance.start({
       config: { ...(this.config || {}), locale },
