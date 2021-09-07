4bee74ba19e5ba894b0c2726ccf5ed7adbaad5fb
Igor Suvorov <hi@isuvorov.com>
2021-09-07 10:13:14 +0300

    feat(env): add instance and version

--- packages/env/src/env.ts
+++ packages/env/src/env.ts
@@ -13,10 +13,21 @@ export const isClient = !isServer;
 export const isDev = (isServer ? process.env.NODE_ENV !== 'production' : Boolean(window.__DEV__)) || false;
 export const isProd = !isDev;
 export const isDebug = isDev;
+
 export const stage =
   (isServer
     ? process.env.STAGE || process.env.__STAGE || process.env.__STAGE__ || (isDev ? process.env.user : null)
     : (window.env && window.env.stage) || window.__STAGE__) || 'development';
+
+export const version = isServer
+  ? process.env.VERSION || process.env.__VERSION || process.env.__VERSION__
+  : (window.env && window.env.version) || '000000';
+
+export const instance = isServer
+  ? process.env.INSTANCE || process.env.__INSTANCE || process.env.__INSTANCE__
+  : (window.env && window.env.instance) || '1';
+
+export const isMasterInstance = instance === '1';
 
 export default {
   isServer,
@@ -25,4 +36,7 @@ export default {
   isProd,
   isDebug,
   stage,
+  version,
+  instance,
+  isMasterInstance,
 };
