1321314ddc768782a94f6a51c4ff6c6387f79a9c
Igor Suvorov <hi@isuvorov.com>
2021-10-11 12:32:40 +0300

    feat(log): remove nanocolors, copy implementation

--- packages/log/src/color.ts
+++ packages/log/src/color.ts
@@ -1,12 +1,11 @@
-import nanocolors from 'nanocolors';
-
+import colors from './colors';
 import { theme } from './config';
 import hashCode from './utils/hashCode';
 
 export function getColors(name: string | null): string[] {
   if (!name) return [];
   // eslint-disable-next-line no-nested-ternary
-  return theme[name] ? theme[name] : nanocolors[name] ? [name] : [];
+  return theme[name] ? theme[name] : colors[name] ? [name] : [];
 }
 
 export function getHashColors(name: string): string[] {
@@ -14,20 +13,20 @@ export function getHashColors(name: string): string[] {
 }
 
 export function hashColor(name: string, str: string): string {
-  const colors = getHashColors(name);
-  return colors.reduce((msg, colorName) => {
+  const hashColors = getHashColors(name);
+  return hashColors.reduce((msg, colorName) => {
     // eslint-disable-next-line no-console
-    if (!nanocolors[colorName]) console.log('!nanocolors[colorName]', { colorName });
-    return nanocolors[colorName](msg);
+    if (!colors[colorName]) console.log('!nanocolors[colorName]', { colorName });
+    return colors[colorName](msg);
   }, str);
 }
 
 export function color(name: string | null, str: string): string {
-  const colors = getColors(name);
-  return colors.reduce((msg, colorName) => {
+  const hashColors = getColors(name);
+  return hashColors.reduce((msg, colorName) => {
     // eslint-disable-next-line no-console
-    if (!nanocolors[colorName]) console.log('!nanocolors[colorName]', { colorName });
-    return nanocolors[colorName](msg);
+    if (!colors[colorName]) console.log('!nanocolors[colorName]', { colorName });
+    return colors[colorName](msg);
   }, str);
 }
 
---
+++ packages/log/src/colors/index.ts
@@ -0,0 +1,3 @@
+export * from './types';
+export * from './colors';
+export { default } from './colors';
