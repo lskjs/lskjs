65e707111a1384f2e65e1534bf4d09fa2c1059c4
Igor Suvorov <hi@isuvorov.com>
2021-10-11 20:20:17 +0300

    feat(log): concat first strings

--- packages/log/src/pretty/prettyFormat.ts
+++ packages/log/src/pretty/prettyFormat.ts
@@ -4,6 +4,21 @@ import { ILogger } from '../types';
 import { isUrlLog } from '../utils/isUrlLog';
 import { prettyContent, prettyLevel, prettyNs, prettyUrl } from './utils';
 
+export function countInRow(args: any[], filter: any): number {
+  let stringsLength = 0;
+  args.forEach((a, pos) => {
+    if (pos !== stringsLength) return;
+    if (filter(a)) stringsLength += 1;
+  });
+  return stringsLength;
+}
+
+export function concatFirstStrings(...args: any[]): string[] {
+  const stringsCount = countInRow(args, (a: any) => typeof a === 'string');
+
+  return [args.slice(0, stringsCount).join(' '), ...args.slice(stringsCount)];
+}
+
 export function prettyFormat(log: ILogger, ...args: any[]): string[] {
   const [mainArg] = args;
   const logArgs = mainArg.data || args;
@@ -11,7 +26,7 @@ export function prettyFormat(log: ILogger, ...args: any[]): string[] {
     return [prettyUrl(mainArg, { level: log.level })];
   }
   const names: string[] = [...(log.ns || '').split(':'), log.name].filter(Boolean).map(String);
-  return [prettyLevel(mainArg.level), prettyNs(names), ...prettyContent(...logArgs)];
+  return concatFirstStrings(prettyLevel(mainArg.level), prettyNs(names), ...prettyContent(...logArgs));
 }
 
 export default prettyFormat;
