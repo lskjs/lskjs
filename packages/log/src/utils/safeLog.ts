2c26da8cd463bf58f7a6b05b514633b904c13a31
Igor Suvorov <hi@isuvorov.com>
2021-10-12 17:38:43 +0300

    feat(log): add log_format to safeLog

--- packages/log/src/utils/safeLog.ts
+++ packages/log/src/utils/safeLog.ts
@@ -1,8 +1,28 @@
+import { isClient, isDev } from '@lskjs/env';
+
+import { env } from './env';
+
+// const LOG_LEVEL = () => env('LOG_LEVEL', '');
+const LOG_FORMAT = () => env('LOG_FORMAT', isDev || isClient ? 'pretty' : 'lsk');
+
 export const safeLog = (ctx: any, level = 'error', ...args: any[]) => {
   if (ctx.log && ctx.log[level]) {
     ctx.log[level](...args);
   } else {
-    console.error(`[${level[0]}] <${ctx.name || ctx.constructor.name}>`, ...args); // eslint-disable-line no-console
+    const logFormat = LOG_FORMAT();
+    if (logFormat === 'none') return;
+    if (logFormat === 'lsk') {
+      // eslint-disable-next-line no-console
+      console.error(
+        {
+          level,
+          name: ctx.name || ctx.constructor.name,
+        },
+        ...args,
+      );
+    } else {
+      console.error(`[${level[0]}] <${ctx.name || ctx.constructor.name}>`, ...args); // eslint-disable-line no-console
+    }
   }
 };
 export default safeLog;
