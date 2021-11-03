a0ff327c49389c078af1f5391cbbbce69a7bee28
Igor Suvorov <hi@isuvorov.com>
2021-11-03 20:53:59 +0300

    fix(proxy): raw proxies

--- packages/proxy/src/providers/raw/RawProvider.js
+++ packages/proxy/src/providers/raw/RawProvider.js
@@ -8,14 +8,17 @@ import { ProxyProvider } from '../ProxyProvider';
 export class RawProvider extends ProxyProvider {
   provider = 'raw';
   async createClient() {
+    if (this.config.proxies) return axios;
     if (!this.config.url) throw new Err('!config.url');
     return axios;
   }
   async fetchListRaw() {
+    if (this.config.proxies) return this.config.proxies;
     const { data } = await this.client.get(this.config.url);
     return data;
   }
   async fetchList() {
+    if (this.config.proxies) return this.config.proxies;
     const data = await this.fetchListRaw();
     const proxyList = parseProxies(data);
     return proxyList.map((proxy) => this.createProxy(proxy));
