e228c9d5e927b09146d46f2ab99ae5d27466fc29
Igor Suvorov <hi@isuvorov.com>
2021-11-10 08:58:30 +0300

    feat(proxy): add urls array to raw provider

--- packages/proxy/src/providers/index.js
+++ packages/proxy/src/providers/index.js
@@ -2,5 +2,4 @@ export default {
   // rsocksnet: () => import('./rsocksnet'),
   proxystore: () => import('./proxystore'),
   raw: () => import('./raw'),
-  raw2: () => import('./raw'),
 };
--- packages/proxy/src/providers/raw/RawProvider.js
+++ packages/proxy/src/providers/raw/RawProvider.js
@@ -1,25 +1,45 @@
 /* eslint-disable camelcase */
 import Err from '@lskjs/err';
 import axios from 'axios';
+import Bluebird from 'bluebird';
 
 import { parseProxies } from '../../utils/parseProxies';
 import { ProxyProvider } from '../ProxyProvider';
 
 export class RawProvider extends ProxyProvider {
   provider = 'raw';
+  getConfigUrls() {
+    const urls = this.config.urls || [];
+    if (this.config.url) urls.push(this.config.url);
+    return urls;
+  }
   async createClient() {
     if (this.config.proxies) return axios;
-    if (!this.config.url) throw new Err('!config.url');
+    const urls = this.getConfigUrls();
+    if (!urls?.length) throw new Err('!config.url');
     return axios;
   }
   async fetchListRaw() {
     if (this.config.proxies) return this.config.proxies;
-    const { data } = await this.client.get(this.config.url);
-    return data;
+    const urls = this.getConfigUrls();
+    return Bluebird.map(
+      urls,
+      async (url) => {
+        try {
+          const { data } = await this.client.get(this.config.url);
+          return data;
+        } catch (err) {
+          this.log.error('[fetchListRaw]', { url }, err);
+          return { err };
+        }
+      },
+      { concurrency: 10 },
+    );
   }
   async fetchList() {
-    const data = await this.fetchListRaw();
-    const proxyList = parseProxies(data);
+    const datas = await this.fetchListRaw();
+    const proxyLists = datas.map((data) => (data.err ? [] : parseProxies(data)));
+    const proxyList = [].concat(...proxyLists);
     return proxyList.map((proxy) => this.createProxy(proxy));
   }
 }
