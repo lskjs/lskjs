2c086c1e74c405b90b5d6c7e35e87d31969f02a8
Igor Suvorov <hi@isuvorov.com>
2021-12-23 19:54:48 +0300

    feat(utils): delay more than 1 month

--- packages/utils/src/delay.ts
+++ packages/utils/src/delay.ts
@@ -1,5 +1,19 @@
-export function delay(duration: number): Promise<void> {
+export const DELAY_MAX_SETTIMEOUT = 1234567890;
+
+export function priomiseDelay(duration: number): Promise<void> {
   return new Promise((resolve) => setTimeout(() => resolve(), duration));
+}
+
+export async function delay(duration: number): Promise<void> {
+  if (duration < DELAY_MAX_SETTIMEOUT) return priomiseDelay(duration);
+  while (duration > 0) {
+    const step = duration > DELAY_MAX_SETTIMEOUT ? DELAY_MAX_SETTIMEOUT : duration;
+    // eslint-disable-next-line no-await-in-loop
+    await priomiseDelay(step);
+    // eslint-disable-next-line no-param-reassign
+    duration -= step;
+  }
+  return undefined;
 }
 
 export default delay;
