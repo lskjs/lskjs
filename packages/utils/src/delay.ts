3cb0653bf6549e1b1218255575751d2b7ea0280e
Igor Suvorov <hi@isuvorov.com>
2021-12-23 19:59:09 +0300

    feat(utils): improve DELAY_MAX_SETTIMEOUT

--- packages/utils/src/delay.ts
+++ packages/utils/src/delay.ts
@@ -1,13 +1,15 @@
-export const DELAY_MAX_SETTIMEOUT = 1234567890;
+export const DELAY_MAX_SETTIMEOUT = 2147483647;
 
 export function priomiseDelay(duration: number): Promise<void> {
   return new Promise((resolve) => setTimeout(() => resolve(), duration));
 }
 
+// NOTE: async while because this - https://stackoverflow.com/questions/3468607/why-does-settimeout-break-for-large-millisecond-delay-values
+
 export async function delay(duration: number): Promise<void> {
   if (duration < DELAY_MAX_SETTIMEOUT) return priomiseDelay(duration);
   while (duration > 0) {
-    const step = duration > DELAY_MAX_SETTIMEOUT ? DELAY_MAX_SETTIMEOUT : duration;
+    const step = duration >= DELAY_MAX_SETTIMEOUT ? DELAY_MAX_SETTIMEOUT : duration;
     // eslint-disable-next-line no-await-in-loop
     await priomiseDelay(step);
     // eslint-disable-next-line no-param-reassign
