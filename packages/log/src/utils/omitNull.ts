9a354b4e9de937e360a45932175013a12724e950
Igor Suvorov <hi@isuvorov.com>
2021-12-23 18:22:10 +0300

    feat(utils): omitNull - allow filter argument

--- packages/log/src/utils/omitNull.ts
+++ packages/log/src/utils/omitNull.ts
@@ -1,6 +1,6 @@
-export const omitNull = (props: { [key: string]: any }) =>
+export const omitNull = (props: { [key: string]: any }, filter = (a: any) => a != null) =>
   Object.keys(props)
-    .filter((k) => props[k] != null)
+    .filter((k) => filter(props[k]))
     .reduce((acc, key) => {
       acc[key] = props[key];
       return acc;
