a660a3491e12caaec6ce3be35390c920f19f500a
Vlados <vlados.pochta@gmail.com>
2022-09-30 20:50:13 +0300

    fix(bots-plugin-notify): add error catcher

--- packages/bots-plugin-notify/src/Api/NotifyApi.js
+++ packages/bots-plugin-notify/src/Api/NotifyApi.js
@@ -5,6 +5,7 @@ import Bluebird from 'bluebird';
 import find from 'lodash/find';
 import get from 'lodash/get';
 import groupBy from 'lodash/groupBy';
+import isEmpty from 'lodash/isEmpty';
 
 import { getBool, getReqParams } from '../utils';
 import { getAlertManagerMessage, getGithubMessage, getGitlabMessage, getGraylogMessage } from './helpers';
@@ -34,6 +35,29 @@ export class NotifyApi extends Api {
     this.sendNotification = sendNotification.bind(notify);
   }
 
+  async sendError({ error, message }) {
+    const errorMessage = JSON.stringify({
+      message: message || `Не удалось собрать message! \n\nСмотри консоль => ${new Date().toISOString()}`,
+      error: {
+        message: error?.response?.data?.message || error?.message,
+        code: error?.response?.data?.code || error?.response?.error_code || error?.code,
+        description: error?.response?.data?.description || error?.response?.description || error?.description,
+      },
+    });
+
+    console.error('[sendError]', errorMessage, error);
+
+    const errorChats = this.notifyConfig.projects?._error?.telegram;
+    if (isEmpty(errorChats)) return;
+
+    await Bluebird.each(errorChats, (errorChat) =>
+      this.bot.telegram.sendMessage(errorChat, errorMessage).catch((err) => {
+        console.error('[sendError.sendMessage]', err);
+        throw err;
+      }),
+    );
+  }
+
   async sendNotifications() {
     const date = Date.now() - this.messageTimeout;
     const { old = [], new2 = [] } = groupBy(this.messages, (m) => (m.createdAt < date ? 'old' : 'new2'));
@@ -54,16 +78,11 @@ export class NotifyApi extends Api {
       });
     }
 
-    try {
-      const res = await Promise.all([sendTelegram, sendSlack]);
-      if (isDev) {
-        return res;
-      }
-      return { data: 'ok' };
-    } catch (err) {
-      console.log({ err });
-      throw new Err(err);
+    const res = await Promise.all([sendTelegram, sendSlack]);
+    if (isDev) {
+      return res;
     }
+    return { status: 'success', code: 200 };
   }
 
   createMessage({ req, projectName }) {
@@ -123,11 +142,18 @@ export class NotifyApi extends Api {
 
     if (project.secret && token !== project.secret) throw new Err('!acl'); // x aceess token
 
-    const message = this.createMessage({ req, projectName });
+    let message;
+    try {
+      message = this.createMessage({ req, projectName });
 
-    this.messages.push(message);
-    await Bluebird.delay(this.messageTimeout * 1.3);
-    return this.sendNotifications();
+      this.messages.push(message);
+      await Bluebird.delay(this.messageTimeout * 1.3);
+      const res = await this.sendNotifications();
+      return res;
+    } catch (error) {
+      await this.sendError({ error, message });
+      return { status: 'error', code: 503 };
+    }
   }
 }
 
