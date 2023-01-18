47c55521667dfe496cd932cc6618c139ad92941d
Igor Suvorov <hi@isuvorov.com>
2023-01-18 18:09:21 +0300

    feat: migrate @lskjs/cli to this repo

--- cli/cli-scripts/scripts/run/release.js
+++ cli/cli-scripts/scripts/run/release.js
@@ -8,7 +8,12 @@ const main = async ({ ctx, args, isRoot } = {}) => {
   if (isRoot) {
     // await shell('lerna version', { ctx, args });
     await shell('lsk run publish --without-publish', { ctx, args });
-    await shell('lerna publish --contents .release', { ctx, args });
+    let cmd = 'lerna publish --contents .release'
+    const isYes = args.includes('--yes');
+    if (isYes) {
+      cmd += ' --yes';
+    }
+    await shell(cmd, { ctx, args });
   } else {
     await shell('npm version prerelease');
     await shell('lsk run publish', { ctx, args });
