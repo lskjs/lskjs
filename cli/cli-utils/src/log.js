0c86e1f8b4da1ccaebe67eafa985bdc1e15d5271
Igor Suvorov <hi@isuvorov.com>
2023-01-18 20:19:11 +0300

    feat: migrate @lskjs/cli to this repo

--- cli/cli-utils/src/log.js
+++ cli/cli-utils/src/log.js
@@ -1,12 +1,17 @@
 // const { Logger, getEnvConfig } = require('@lskjs/log');
+const { omitNull } = require('@lskjs/algos');
 const { Logger, getEnvConfig } = require('@lskjs/log');
 
-const createLogger = (props = {}) =>
-  new Logger({
+const createLogger = (props = {}) => {
+  const config = omitNull({
     ...getEnvConfig(),
     ns: 'cli',
     ...props,
   });
+  // eslint-disable-next-line no-console
+  if (process.env.DEBUG_DEBUG) console.log('[createLogger]', config);
+  return new Logger(config);
+};
 
 const log = createLogger();
 
