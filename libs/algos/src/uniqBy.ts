e402bc1fd9789a7481a0d0167b87fe6ded44b5f9
Igor Suvorov <hi@isuvorov.com>
2023-02-05 16:25:53 +0300

    fix(algos): fix bug

--- libs/algos/src/uniqBy.ts
+++ libs/algos/src/uniqBy.ts
@@ -2,6 +2,7 @@ import { ObjectMapper } from './types';
 
 export type Mapper<T> = (value?: T, key?: string, arr?: Array<T>) => any;
 
+
 export const uniqBy = <T>(arr: Array<T>, mapper: ObjectMapper<T>): Array<T> =>
   // @ts-ignore
   [...new Set(arr.map(mapper))].map((x) => arr.find((y) => mapper(y) === x));
