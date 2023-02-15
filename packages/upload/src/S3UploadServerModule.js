cb6bf2ebf8fb416a25544daee484f352a46d24ed
Andrey Kondalov <andrey@kondalov.com>
2023-02-15 15:09:31 +0200

    fix(upload): detect MIME type from buffer

--- packages/upload/src/S3UploadServerModule.js
+++ packages/upload/src/S3UploadServerModule.js
@@ -1,6 +1,8 @@
 import Err from '@lskjs/err';
 import aws from 'aws-sdk';
+import { filetypeextension, filetypemime } from 'magic-bytes.js';
 import multerS3 from 'multer-s3';
+import stream from 'stream';
 
 import { UploadServerModule } from './UploadServerModule';
 
@@ -14,11 +16,50 @@ export class S3UploadServerModule extends UploadServerModule {
     this.storage = this.getStorage();
   }
 
+  isSvg(svg) {
+    const svgRegex = /^\s*(?:<\?xml[^>]*>\s*)?(?:<!doctype svg[^>]*>\s*)?<svg[^>]*>/i;
+    return svgRegex.test(svg);
+  }
+
+  normalizeContentType(req, file, cb) {
+    file.stream.once('data', (firstChunk) => {
+      const _mime = filetypemime(firstChunk);
+      const _ext = filetypeextension(firstChunk);
+      const type =
+        Array.isArray(_mime) && _mime.length
+          ? {
+              mime: _mime[0],
+              ext: _ext[0],
+            }
+          : null;
+
+      let mimetype = type?.mime;
+      let mime = 'application/octet-stream'; // default type
+
+      if ((!type || type.ext === 'xml') && this.isSvg(firstChunk.toString())) {
+        mimetype = 'image/svg+xml';
+      } else if (['text/csv'].includes(file?.mimetype)) {
+        mimetype = file.mimetype;
+      } else if (type) {
+        mime = type.mime;
+      }
+
+      const isMIMETypeValid = this.validateMIMEType(mimetype, cb);
+      if (!isMIMETypeValid) return;
+
+      const outStream = new stream.PassThrough();
+      outStream.write(firstChunk);
+      file.stream.pipe(outStream);
+
+      cb(null, mime, outStream);
+    });
+  }
+
   getStorage() {
     return multerS3({
       s3: this.s3,
       bucket: this.config.s3.bucket,
-      contentType: multerS3.AUTO_CONTENT_TYPE,
+      contentType: this.normalizeContentType.bind(this),
       acl: this.config.s3.acl || 'public-read',
       key: async (req, file, cb) => {
         let filename;
