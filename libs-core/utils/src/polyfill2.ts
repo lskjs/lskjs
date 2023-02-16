9493b3a54b32abd1e6534f55d357138c74db08d4
Igor Suvorov <hi@isuvorov.com>
2023-02-16 17:32:53 +0300

    feat(utils): add polyfill2 withoud dev console.logs

---
+++ libs-core/utils/src/polyfill2.ts
@@ -0,0 +1,28 @@
+/* eslint-disable no-undef */
+/* eslint-disable no-console */
+// import { isDev, stage } from '@lskjs/env';
+
+import global from './global';
+
+// if (__DEV__) {
+//   console.log(`Compiling ... [__STAGE__=${__STAGE__} __DEV__=${__DEV__}]`);
+// }
+global.startedAt = Date.now();
+global.timing = () => Date.now() - global.startedAt;
+
+if (typeof process !== 'undefined') {
+  process.on('uncaughtException', (err) => {
+    console.error("===> process.on('uncaughtException') <===");
+    console.error(err);
+    console.error("^^^^ process.on('uncaughtException') ^^^^");
+  });
+}
+
+if (!global._babelPolyfill) require('@babel/polyfill');
+
+export default function ready(cb) {
+  // if (isDev) {
+  //   console.log(`🔥  Compiled [${global.timing()}ms] [stage=${stage} isDev=${isDev}]`);
+  // }
+  if (typeof cb === 'function') cb();
+}
