81a1f1f16676b42681d5f9ee3b2be7ba3705cf26
Igor Suvorov <hi@isuvorov.com>
2023-04-20 09:13:00 +0300

    fix(colors): isSupported type

--- libs/colors/src/isSupported.ts
+++ libs/colors/src/isSupported.ts
@@ -6,12 +6,14 @@ const envs = isClient ? window : process.env;
 const argv = isServer ? process.argv : [];
 
 export const checkSupport = (): boolean =>
-  !(getEnvVar('NO_COLOR') || argv.includes('--no-color')) && // disable colors when NO_COLOR is set or --no-color in nodejs args
-  (isClient || // enable colors in browser
-    'FORCE_COLOR' in envs || // enable colors when FORCE_COLOR is set
-    argv.includes('--color') || // enable colors when --color in nodejs args
-    (isTTY && process.env.TERM !== 'dumb') || // enable colors when TTY and TERM
-    getEnvVar('CI')); // enable colors when CI is set
+  Boolean(
+    !(getEnvVar('NO_COLOR') || argv.includes('--no-color')) && // disable colors when NO_COLOR is set or --no-color in nodejs args
+      (isClient || // enable colors in browser
+        'FORCE_COLOR' in envs || // enable colors when FORCE_COLOR is set
+        argv.includes('--color') || // enable colors when --color in nodejs args
+        (isTTY && process.env.TERM !== 'dumb') || // enable colors when TTY and TERM
+        getEnvVar('CI')), // enable colors when CI is set
+  );
 
 export const isSupported = checkSupport();
 
