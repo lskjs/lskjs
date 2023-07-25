260637e060e7e5703e604e5b7b4a5acf4902cd27
Igor Suvorov <hi@isuvorov.com>
2023-07-25 20:57:55 +0300

    fix(nest-utils): bug fixes, and migrate nest-config outside

---
+++ libs/nest-utils/src/Time.decorator.ts
@@ -0,0 +1,29 @@
+import { createLogger } from '@lskjs/log';
+
+const log = createLogger('Time', { ns: 'time' });
+
+export function Time(initKey?: string, initLog = log) {
+  return function (
+    target: Record<string, any>,
+    propertyKey: string,
+    descriptor: PropertyDescriptor,
+  ) {
+    const method = descriptor.value;
+    // eslint-disable-next-line no-param-reassign
+    descriptor.value = async function (...args: any[]) {
+      const startedAt = Date.now();
+      const response = await method.apply(this, args);
+      const finishedAt = Date.now();
+      const time = finishedAt - startedAt;
+
+      const key = initKey || `${this.constructor.name}.${propertyKey}`;
+      const str = `${key} ${time}ms`;
+      // if (this.log) {
+      //   this.log.trace(`[Time] ${key} ${time}ms`);
+      // } else {
+      log.trace(str);
+      // }
+      return response;
+    };
+  };
+}
