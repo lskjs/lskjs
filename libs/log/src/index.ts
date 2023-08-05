459bb4c63232fb3b01eb2d26271fa2c92d44a211
Igor Suvorov <hi@isuvorov.com>
2023-08-05 12:48:25 +0300

    fix(module): fix tests with import

--- libs/log/src/index.ts
+++ libs/log/src/index.ts
@@ -2,6 +2,7 @@ export * from './types';
 export * from './Logger';
 export * from './getEnvConfig';
 export * from './createLogger';
+export * from './types';
 // export * from './log';
 
 export { default } from './Logger';
