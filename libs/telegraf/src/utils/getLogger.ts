ef0bc3cb3975a5ec7c5798216368b54414f48bdd
Igor Suvorov <hi@isuvorov.com>
2023-09-20 15:58:51 +0300

    feat(telegraf): improve telegraf utils

--- libs/telegraf/src/utils/getLogger.ts
+++ libs/telegraf/src/utils/getLogger.ts
@@ -1,8 +1,9 @@
 import { createLogger, ILogger } from '@lskjs/log';
 
 const loggers: Record<string, ILogger> = {};
-export const getLogger = (botInfo: any) => {
-  const botUsername = botInfo?.username || botInfo?.id;
-  if (!loggers[botUsername]) loggers[botUsername] = createLogger(`bot:${botUsername}`);
+export const getLogger = (botInfo: { id?: number | string; username?: string }) => {
+  const botUsername = botInfo?.username || botInfo?.id || 'unknown';
+  const name = `bot:${botUsername}`;
+  if (!loggers[botUsername]) loggers[botUsername] = createLogger(name);
   return loggers[botUsername];
 };
