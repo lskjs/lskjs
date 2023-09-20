321589501cb9eaab87539b04b8971c49c56ec59f
Igor Suvorov <hi@isuvorov.com>
2023-09-20 15:33:43 +0300

    feat(cli-scripts): build-ts working directly with ts-node

--- libs-cli/cli-scripts/scripts/run/build-ts.js
+++ libs-cli/cli-scripts/scripts/run/build-ts.js
@@ -1,8 +1,9 @@
 #!/usr/bin/env node
 const { run, shell, shellParallel, getCwdInfo, findBin } = require('@lskjs/cli-utils');
 const { isCI, isDev } = require('@lskjs/env');
+// const { log } = require('@lskjs/log/log');
 
-async function main({ isRoot, ctx, cwd, args } = {}) {
+async function main({ isRoot, ctx, cwd, args, log } = {}) {
   if (isRoot) {
     await shellParallel(`lsk run buiild:ts`, { ctx, args });
     return;
@@ -11,13 +12,24 @@ async function main({ isRoot, ctx, cwd, args } = {}) {
   const isWatch = args.includes('--watch');
   const isProd = !isDev || args.includes('--prod');
   const { isLib } = getCwdInfo({ cwd });
-  let cmd = findBin('tsup');
-  cmd += ' src';
-  if (isSilent) cmd += ' --silent';
-  if (isWatch) cmd += ' --watch';
+  let cmd = '';
+  if (isLib) {
+    cmd = findBin('tsup');
+    cmd += ' src';
+    if (isSilent) cmd += ' --silent';
+    if (isWatch) cmd += ' --watch';
+  } else {
+    const path = 'src/**';
+    const ext = 'ts,tsx,js,jsx,mjs,cjs,json';
+    cmd = 'ts-node src/index.ts';
+    log.trace('watching path:', path);
+    log.trace('watching extensions:', ext);
+    log.debug('to restart at any time, enter `rs`');
+    // 'nodemon --watch "src/**" --ext "ts,json" --ignore "src/**/*.spec.ts" --exec "ts-node src/index.ts"';
+    cmd = `nodemon --watch "${path}" --ext "${ext}" --exec "${cmd}" --quiet`;
+  }
   if (isProd) cmd = `NODE_ENV=production ${cmd}`;
-  // if (isProd) cmd += ' --env.NODE_ENV production';
-  if (isWatch && !isLib) cmd += ' --onSuccess "node lib"';
+
   await shell(cmd, { ctx });
 }
 
