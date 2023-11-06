29babfd16b7159fa24b90d5bdc83a1795ecb50b5
Igor Suvorov <hi@isuvorov.com>
2023-11-06 20:51:52 +0300

    chore: try to fix ci

--- .lskjs.js
+++ .lskjs.js
@@ -1,9 +1,19 @@
-module.exports = {
+module.exports = process.env.STAGE === 'isuvorov' ? {
   pathexec: {
     paths: [
       // `/opt/homebrew/lib/node_modules/lsk/node_modules/@lskjs/cli-scripts`,
       `${__dirname}/libs-cli/cli-scripts`
     ],
   },
+  cwd: __dirname,
+} : process.env.STAGE === 'ci' ? {
+  // pathexec: {
+  //   paths: [
+  //     // `/opt/homebrew/lib/node_modules/lsk/node_modules/@lskjs/cli-scripts`,
+  //     `/lsk/libs-cli/cli-scripts`
+  //   ],
+  // },
+  cwd: __dirname,
+} : {
   cwd: __dirname,
 };
