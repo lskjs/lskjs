89390d3e8c44854496447e833946392e7d2b9f0c
Igor Suvorov <hi@isuvorov.com>
2023-11-06 04:44:23 +0300

    fix(cli-scripts): remove --prod --silent from build & test by default

--- libs-presets/jest-config/strict/jest.setup.js
+++ libs-presets/jest-config/strict/jest.setup.js
@@ -3,7 +3,7 @@
 jest.setTimeout(15000);
 const args = process.argv.slice(2);
 
-const isProd = !isDev || !!process.env.LSK_PROD || args.includes('--prod');
+const isProd = !!process.env.LSK_PROD || args.includes('--prod');
 const isSilent = !!process.env.LSK_SILENT || args.includes('--silent') || isCI;
 
 if (isProd || isSilent) {
